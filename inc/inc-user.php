81ac6aef92c6eeee51ba673e5aa97b10f5b8f423
swling <tangfou@gmail.com>
2019-02-25 13:00:25 +0800

    新增：wnd_mail 站内信发送函数

--- inc/inc-user.php
+++ inc/inc-user.php
@@ -4,7 +4,6 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
-
 /**
  *@since 初始化 判断当前用户是否为管理员
  *@return bool
@@ -57,4 +56,32 @@ function wnd_is_name_repeated($display_name, $exclude_id = 0) {
 	}
 
 	return $value;
+}
+
+/**
+ *@since 2019.02.25
+ *发送站内信
+ */
+function wnd_mail($to, $subject, $message) {
+
+	if (!get_user_by('id', $to)) {
+		return array('status' => 0, 'msg' => '用户不存在！');
+	}
+
+	$postarr = array(
+		'post_type' => 'mail',
+		'post_author' => $to,
+		'post_title' => $subject,
+		'post_content' => $message,
+		'post_status' => 'pending',
+	);
+
+	$mail_id = wp_insert_post($postarr);
+
+	if (is_wp_error($mail_id)) {
+		return array('status' => 0, 'msg' => $mail_id->get_error_message());
+	} else {
+		return array('status' => 1, 'msg' => '发送成功！');
+	}
+
 }
