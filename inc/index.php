c400ba40435327ebd6dc83e7938be063ce718f0b
swling <tangfou@gmail.com>
2019-02-26 10:54:15 +0800

    新增term模板

--- inc/index.php
+++ inc/index.php
@@ -30,6 +30,7 @@ require WNDWP_PATH . 'inc/add-filter.php'; //添加的钩子
 require WNDWP_PATH . 'template/module.php'; //相对独立的模块模板
 require WNDWP_PATH . 'template/user.php'; //user模板
 require WNDWP_PATH . 'template/post.php'; //post模板
+require WNDWP_PATH . 'template/term.php'; //term模板
 require WNDWP_PATH . 'template/archive.php'; //归档和列表模板
 require WNDWP_PATH . 'template/finance.php'; //财务模板
 require WNDWP_PATH . 'template/admin.php'; //前端管理模板
---
+++ template/term.php
@@ -0,0 +1,57 @@
+<?php
+
+//###################################################### 生成taxonomy 多重查询参数 默认 GET name 为 term_id, GET值为 id
+function wnd_terms_query_arg($query = array(), $key = 'term_id', $remove_key = '', $title = '全部') {
+
+	if (!$query) {
+		return false;
+	}
+
+	$terms = get_terms($query);
+	$current_term_id = isset($_GET[$key]) ? $_GET[$key] : false;
+	$all_class = !$current_term_id ? 'class="on"' : false;
+
+	if ($terms) {
+		echo '<ul class="' . $query['taxonomy'] . '">';
+		array_push($remove_key, $key);
+		echo '<li><a href="' . remove_query_arg($remove_key) . '" ' . $all_class . ' >' . $title . '</a></li>';
+		foreach ($terms as $term) {
+
+			$current = ($current_term_id == $term->term_id) ? 'class="on"' : null;
+			echo '<li><a href="' . add_query_arg($key, $term->term_id, $url = remove_query_arg($remove_key)) . '" ' . $current . '>' . $term->name . '</a></li>' . PHP_EOL;
+
+		}
+		unset($term);
+		echo '</ul>';
+	}
+
+}
+
+/*
+在文章内容比较少的情况下，通过数据库查询的方式获取当前term关联的其他term生成多重查询参数
+目的：避免产生过多没有内容的多重筛选
+@2018.12.14 首次封装
+ */
+function wnd_related_terms_query_arg($current_term_id, $related_taxonmy, $key, $remove_key = array(), $title = 'All', $query_post_max = 1000) {
+
+	$current_term = get_term($current_term_id);
+
+	if ($current_term->count < $query_post_max) {
+
+		$terms = wndbiz_get_related_terms($term_id = $current_term_id, $terms_type = $related_taxonmy);
+		if ($terms) {
+			$all_class = isset($_GET[$key]) ? '' : 'class="on"';
+
+			array_push($remove_key, $key);
+			echo '<ul class="' . $related_taxonmy . '">';
+			echo '<li><a href="' . remove_query_arg($remove_key) . '" ' . $all_class . ' >' . $title . '</a></li>';
+			foreach ($terms as $term) {
+				$is_active = $_GET[$key] == $term->term_id ? 'class="is-active"' : '';
+				echo '<li><a href="' . add_query_arg($key, $term->term_id, $url = remove_query_arg($remove_key)) . '"  ' . $is_active . '>' . $term->name . '</a></li>';
+			}
+			unset($term);
+			echo '</ul>';
+		}
+	}
+
+}
