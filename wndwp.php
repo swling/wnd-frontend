53e54147b7d4020d21902a8352941e6f5f0e287f
swling <tangfou@gmail.com>
2019-03-14 17:28:05 +0800

    取消文章field白名单设置

--- wndwp.php
+++ wndwp.php
@@ -55,7 +55,7 @@ function wnd_install() {
 			$default_option = array(
 
 				'wnd_enable_white_list' => 1,
-				'wnd_allowed_post_field' => 'post_title,post_excerpt,post_content,post_parent',
+				// 'wnd_allowed_post_field' => 'post_title,post_excerpt,post_content,post_parent',
 				'wnd_allowed_post_meta_key' => '',
 				'wnd_allowed_wp_post_meta_key' => 'price',
 				'wnd_allowed_user_meta_key' => '',
