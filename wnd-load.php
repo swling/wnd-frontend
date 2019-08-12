5a63f40a395b384adf4071d3eac3f8ccfed64296
swling <tangfou@gmail.com>
2019-08-12 13:32:33 +0800

    自动加载大小写优化

--- wnd-load.php
+++ wnd-load.php
@@ -54,6 +54,7 @@ require WND_PATH . 'templates/tpl-gallery.php'; //橱窗相册
  */
 function wnd_class_loader($class) {
 	$file_name = 'class-' . str_replace('_', '-', $class);
+	$file_name = strtolower($file_name);
 	$file = __DIR__ . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . $file_name . '.php';
 	if (file_exists($file)) {
 		require $file;
