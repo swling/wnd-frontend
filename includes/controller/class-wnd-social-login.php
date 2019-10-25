236c43eb84054d881004ece70af7f04e419dd8bc
swling <tangfou@gmail.com>
2019-10-25 12:47:59 +0800

    社交登录优化：增强内聚

--- includes/controller/class-wnd-social-login.php
+++ includes/controller/class-wnd-social-login.php
@@ -40,18 +40,6 @@ abstract class Wnd_Social_Login {
 	abstract public function build_oauth_url($return_url);
 
 	/**
-	 *根据第三方平台用户信息登录或创建账户
-	 */
-	public function login() {
-		$this->get_token();
-		$this->get_open_id();
-		$this->get_user_info();
-
-		// 根据open id创建或登录账户
-		wnd_social_login($this->open_id, $this->display_name, $this->avatar_url);
-	}
-
-	/**
 	 *根据用户授权码获取token
 	 */
 	abstract protected function get_token();
@@ -65,4 +53,70 @@ abstract class Wnd_Social_Login {
 	 *根据token和open id获取用户信息
 	 */
 	abstract protected function get_user_info();
+
+	/**
+	 *根据第三方平台用户信息登录或创建账户
+	 */
+	public function login() {
+		$this->get_token();
+		$this->get_open_id();
+		$this->get_user_info();
+
+		// 根据open id创建或登录账户
+		self::wnd_social_login($this->open_id, $this->display_name, $this->avatar_url);
+	}
+
+	/**
+	 *@since 2019.07.23
+	 *根据第三方网站获取的用户信息，注册或者登录到WordPress站点
+	 *@param string $open_id 		第三方账号openID
+	 *@param string $display_name 	用户名称
+	 *@param string $avatar_url 	用户外链头像
+	 *
+	 **/
+	public static function wnd_social_login($open_id, $display_name = '', $avatar_url = '') {
+		//当前用户已登录，同步信息
+		if (is_user_logged_in()) {
+			$this_user   = wp_get_current_user();
+			$may_be_user = wnd_get_user_by_openid($open_id);
+			if ($may_be_user and $may_be_user->ID != $this_user->ID) {
+				exit('OpenID已被其他账户占用！');
+			}
+
+			if ($avatar_url) {
+				wnd_update_user_meta($this_user->ID, "avatar_url", $avatar_url);
+			}
+			if ($open_id) {
+				wnd_update_user_openid($this_user->ID, $open_id);
+			}
+			wp_redirect(wnd_get_option('wnd', 'wnd_reg_redirect_url') ?: home_url());
+			exit;
+		}
+
+		//当前用户未登录，注册或者登录 检测是否已注册
+		$user = wnd_get_user_by_openid($open_id);
+		if (!$user) {
+
+			// 自定义随机用户名
+			$user_login = wnd_generate_login();
+			$user_pass  = wp_generate_password();
+			$user_array = array('user_login' => $user_login, 'user_pass' => $user_pass, 'display_name' => $display_name);
+			$user_id    = wp_insert_user($user_array);
+
+			// 注册
+			if (is_wp_error($user_id)) {
+				wp_die($user_id->get_error_message(), get_option('blogname'));
+			} else {
+				wnd_update_user_openid($user_id, $open_id);
+			}
+		}
+
+		// 获取用户id
+		$user_id = $user ? $user->ID : $user_id;
+
+		wnd_update_user_meta($user_id, "avatar_url", $avatar_url);
+		wp_set_auth_cookie($user_id, true);
+		wp_redirect(wnd_get_option('wnd', 'wnd_reg_redirect_url') ?: home_url());
+		exit();
+	}
 }
