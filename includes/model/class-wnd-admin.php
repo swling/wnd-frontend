3b4daa109d95037be6a2face428bf6378ab70060
swling <tangfou@gmail.com>
2019-12-16 14:30:49 +0800

    MySQL函数 DATE_SUB删除参数首位空格

--- includes/model/class-wnd-admin.php
+++ includes/model/class-wnd-admin.php
@@ -98,14 +98,14 @@ class Wnd_Admin {
 		global $wpdb;
 
 		// 一年前的站内信
-		$old_posts = $wpdb->get_col("SELECT ID FROM $wpdb->posts WHERE post_type = 'mail' AND DATE_SUB( NOW(), INTERVAL 365 DAY ) > post_date");
+		$old_posts = $wpdb->get_col("SELECT ID FROM $wpdb->posts WHERE post_type = 'mail' AND DATE_SUB(NOW(), INTERVAL 365 DAY) > post_date");
 		foreach ((array) $old_posts as $delete) {
 			// Force delete.
 			wp_delete_post($delete, true);
 		}
 
 		// 一年前的充值记录及非产品订单
-		$old_posts = $wpdb->get_col("SELECT ID FROM $wpdb->posts WHERE post_type IN ('order','recharge') AND post_parent = 0 AND DATE_SUB( NOW(), INTERVAL 365 DAY ) > post_date");
+		$old_posts = $wpdb->get_col("SELECT ID FROM $wpdb->posts WHERE post_type IN ('order','recharge') AND post_parent = 0 AND DATE_SUB(NOW(), INTERVAL 365 DAY) > post_date");
 		foreach ((array) $old_posts as $delete) {
 			// Force delete.
 			wp_delete_post($delete, true);
@@ -113,7 +113,7 @@ class Wnd_Admin {
 
 		// 超期七天未完成的充值消费订单
 		$old_posts = $wpdb->get_col(
-			"SELECT ID FROM $wpdb->posts WHERE post_type IN ('order','recharge') AND post_status = 'pending' AND DATE_SUB( NOW(), INTERVAL 7 DAY ) > post_date"
+			"SELECT ID FROM $wpdb->posts WHERE post_type IN ('order','recharge') AND post_status = 'pending' AND DATE_SUB(NOW(), INTERVAL 7 DAY) > post_date"
 		);
 		foreach ((array) $old_posts as $delete) {
 			// Force delete.
