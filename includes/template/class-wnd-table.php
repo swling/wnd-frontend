5f713cad32e90385617a1e861701802f1600dd7c
swling <tangfou@gmail.com>
2019-12-19 16:50:13 +0800

    修复未定义财务统计列表的bug

--- includes/template/class-wnd-table.php
+++ includes/template/class-wnd-table.php
@@ -15,6 +15,7 @@ class Wnd_Table {
 	 */
 	public static function build(\WP_Query $query) {
 		$method = 'build_' . $query->query_vars['post_type'] . '_table';
+		$method = str_replace('-', '_', $method);
 		if (method_exists(__CLASS__, $method)) {
 			return self::$method($query);
 		} else {
@@ -89,6 +90,28 @@ class Wnd_Table {
 	 *@return 	string 	$html 	输出表单
 	 */
 	protected static function build_recharge_table($query) {
+		return self::build_finance_table($query);
+	}
+
+	/**
+	 *@since 2019.03.14
+	 *以表格形式输出用户消费记录
+	 *
+	 *@param 	object 	$query 	WP_Query 实例化结果
+	 *@return 	string 	$html 	输出表单
+	 */
+	protected static function build_order_table($query) {
+		return self::build_finance_table($query);
+	}
+
+	/**
+	 *@since 2019.03.14
+	 *以表格形式输出用户消费/充值记录
+	 *
+	 *@param 	object 	$query 	WP_Query 实例化结果
+	 *@return 	string 	$html 	输出表单
+	 */
+	protected static function build_finance_table($query) {
 		$table = new Wnd_Posts_Table($query, true, true);
 		$table->add_column(
 			[
@@ -113,7 +136,7 @@ class Wnd_Table {
 		);
 		$table->add_column(
 			[
-				'post_field' => 'post_title',
+				'post_field' => ('order' == $query->query_vars['post_type']) ? 'post_parent_with_link' : 'post_title',
 				'title'      => '详情',
 				'class'      => 'is-narrow',
 			]
@@ -130,25 +153,43 @@ class Wnd_Table {
 	}
 
 	/**
-	 *@since 2019.03.14
-	 *以表格形式输出用户消费记录
+	 *@since 2019.12.19
+	 *
+	 *消费统计列表
 	 *
 	 *@param 	object 	$query 	WP_Query 实例化结果
 	 *@return 	string 	$html 	输出表单
-	 */
-	protected static function build_order_table($query) {
+	 **/
+	protected static function build_stats_ex_table($query) {
+		return self::build_finance_stats_table($query);
+	}
+
+	/**
+	 *@since 2019.12.19
+	 *
+	 *充值统计列表
+	 *
+	 *@param 	object 	$query 	WP_Query 实例化结果
+	 *@return 	string 	$html 	输出表单
+	 **/
+	protected static function build_stats_re_table($query) {
+		return self::build_finance_stats_table($query);
+	}
+
+	/**
+	 *@since 2019.12.19
+	 *
+	 *财务统计列表
+	 *
+	 *@param 	object 	$query 	WP_Query 实例化结果
+	 *@return 	string 	$html 	输出表单
+	 **/
+	protected static function build_finance_stats_table($query) {
 		$table = new Wnd_Posts_Table($query, true, true);
 		$table->add_column(
 			[
-				'post_field' => 'post_date',
-				'title'      => '日期',
-				'class'      => 'is-narrow is-hidden-mobile',
-			]
-		);
-		$table->add_column(
-			[
-				'post_field' => 'post_author',
-				'title'      => '用户',
+				'post_field' => 'post_title',
+				'title'      => '标题',
 				'class'      => 'is-narrow',
 			]
 		);
@@ -156,24 +197,17 @@ class Wnd_Table {
 			[
 				'post_field' => 'post_content',
 				'title'      => '金额',
-				'class'      => 'is-narrow',
 			]
 		);
 		$table->add_column(
 			[
-				'post_field' => 'post_parent_with_link',
-				'title'      => '详情',
-				'class'      => 'is-narrow',
-			]
-		);
-		$table->add_column(
-			[
-				'post_field' => 'post_status',
-				'title'      => '状态',
+				'post_field' => 'post_date',
+				'title'      => '创建时间',
 				'class'      => 'is-narrow is-hidden-mobile',
 			]
 		);
 		$table->build();
-		return $table->html;
+		$html = $table->html;
+		return $html;
 	}
 }
