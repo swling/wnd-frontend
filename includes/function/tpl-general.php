f86fe84aa36ebdd378ed135cc12a63fd9a62c368
swling <tangfou@gmail.com>
2020-03-27 10:57:32 +0800

    wnd_message新增class选择器

--- includes/function/tpl-general.php
+++ includes/function/tpl-general.php
@@ -225,7 +225,7 @@ function wnd_paid_download_button($post_id) {
  *@since 2020.03.22
  */
 function wnd_message($message, $color = '', $centered = false) {
-	$class = 'message content';
+	$class = 'message content wnd-message';
 	$class .= $color ? ' ' . $color : '';
 	$class .= $centered ? ' has-text-centered' : '';
 
