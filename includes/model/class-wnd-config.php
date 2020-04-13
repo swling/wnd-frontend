54d8884beae1f5ba16e476dec43c89f05ecd1c9f
swling <tangfou@gmail.com>
2020-04-13 10:34:47 +0800

    静态变量引用使用 static:: 关键词，以便子类重写静态变量

--- includes/model/class-wnd-config.php
+++ includes/model/class-wnd-config.php
@@ -19,13 +19,15 @@ class Wnd_Config {
 
 	/**
 	 *获取指定option数组中的指定键值
+	 *
+	 *此处引用静态变量应该使用 static::关键词，否则继承子类无法重写静态变量
 	 */
 	public static function get($config_key) {
-		if (0 !== stripos($config_key, self::$config_key_prefix)) {
-			$config_key = self::$config_key_prefix . $config_key;
+		if (0 !== stripos($config_key, static::$config_key_prefix)) {
+			$config_key = static::$config_key_prefix . $config_key;
 		}
 
-		$config       = get_option(self::$wp_option_name, []);
+		$config       = get_option(static::$wp_option_name, []);
 		$config_value = $config[$config_key] ?? false;
 
 		return apply_filters($config_key, $config_value);
