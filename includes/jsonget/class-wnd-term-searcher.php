4604e8aa652658573408dd2e332fbe7baa7cf9d1
swling <tangfou@gmail.com>
2020-05-14 13:04:25 +0800

    修复：Wnd_Term_Searcher 限定数目无效的bug

--- includes/jsonget/class-wnd-term-searcher.php
+++ includes/jsonget/class-wnd-term-searcher.php
@@ -10,13 +10,15 @@ use Wnd\Model\Wnd_Term;
  *@link https://developer.wordpress.org/reference/classes/WP_Term_Query/__construct/
  *@link https://developer.wordpress.org/reference/classes/wp_term_query/get_search_sql/
  *
+ *@see class WP_Term_Query
+ *如果需要限制返回结果数目，即设置number参数，必须确保parent参数为 空\false，否则number参数将无效
  */
 class Wnd_Term_Searcher extends Wnd_JsonGet {
 
 	public static function get($args = []) {
 		$defaults = [
-			'taxonomy'   => 'category',
-			'parent'     => 0,
+			'taxonomy'   => 'post_tag',
+			'parent'     => '',
 			'hide_empty' => false,
 			'orderby'    => 'count',
 			'order'      => 'DESC',
