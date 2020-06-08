548ee21326117f18b1348ee94b94c67ecbbf274f
Swling <tangfou@gmail.com>
2020-06-08 12:21:45 +0800

    新增Excel处理类：Wnd_Excel

---
+++ includes/utility/wnd-excel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Wnd\Utility;
+
+class Wnd_Excel {
+
+	/**
+	 * 将数组作为csv导出
+	 * @param $data
+	 * @param $filename
+	 */
+	public static function array_to_csv(array $data, string $filename = '') {
+		$filename = $filename ?: date('Y-m-d:h-i-s', time());
+		if (!$data) {
+			return;
+		}
+
+		header('Content-Type: application/vnd.ms-execl');
+		header('Content-Disposition: attachment;filename="' . $filename . '.csv"');
+		header("Pragma: no-cache");
+		header("Expires: 0");
+
+		//以写入追加的方式打开
+		$fp = fopen('php://output', 'a');
+
+		$i = 0;
+		foreach ($data as $k => $v) {
+			fputcsv($fp, $v);
+			$i++;
+
+			//读取一部分数据刷新下输出buffer
+			if ($i > 1000) {
+				ob_flush();
+				flush();
+				$i = 0;
+			}
+		}
+		unset($k, $v);
+
+		fclose($fp);
+	}
+}
