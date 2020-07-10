2e5201e0da2b23b226c04d52bffc42607034a8a8
Swling <tangfou@gmail.com>
2020-07-10 11:29:34 +0800

    更新包托管至：wndwp.com

--- includes/utility/wnd-upgrader-plugin-this.php
+++ includes/utility/wnd-upgrader-plugin-this.php
@@ -19,24 +19,15 @@ class Wnd_Upgrader_Plugin_This extends Wnd_Upgrader_Plugin {
 	 *	$this->upgrade_info['new_version'];
 	 */
 	protected function get_remote_info() {
-		// $url      = 'https://api.github.com/repos/swling/wnd-frontend/releases';
-		// $response = wp_remote_get($url, ['headers' => ['Authorization' => 'token 2211ea551d8dc314ce715bae0b3ee5cd1de8d6b7']]);
-		// if (is_wp_error($response)) {
-		// 	return $response;
-		// }
+		$url      = 'https://wndwp.com/wp-json/wndt/project/157';
+		$response = wp_remote_get($url, ['headers' => ['Authorization' => 'token xxx']]);
+		if (is_wp_error($response)) {
+			return $response;
+		}
+		$response = json_decode($response['body'], true);
 
-		// $response = json_decode($response['body'], true);
-		// if (is_array($response)) {
-		// 	$response = current($response);
-		// }
-
-		$response['tag_name']    = '0.01';
-		$response['html_url']    = 'http://127.0.0.1/wordpress';
-		$response['zipball_url'] = 'http://127.0.0.1/wordpress.zip';
-
-		// 构造安装包信息
-		$this->upgrade_info['url']         = $response['html_url'];
-		$this->upgrade_info['package']     = $response['zipball_url'];
-		$this->upgrade_info['new_version'] = $response['tag_name'];
+		if ($response['status'] > 0) {
+			$this->upgrade_info = array_merge($this->upgrade_info, $response['data']);
+		}
 	}
 }
