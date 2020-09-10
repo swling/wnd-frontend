6f2ccb43965643d7292e52c783cd8b75eef08719
Swling <tangfou@gmail.com>
2020-09-10 15:33:31 +0800

    Action 层数据提交统一由 Wnd_Form_Data 处理；Wnd_Form_Data 加入人机验证选项

--- includes/action/wnd-action-ajax.php
+++ includes/action/wnd-action-ajax.php
@@ -1,6 +1,8 @@
 <?php
 namespace Wnd\Action;
 
+use Wnd\Utility\Wnd_Form_Data;
+
 /**
  *@since 2019.10.02
  *Ajax操作基类
@@ -11,4 +13,13 @@ abstract class Wnd_Action_Ajax {
 	 *获取全局变量并选择model执行
 	 */
 	abstract public static function execute(): array;
+
+	/**
+	 *@since 0.8.64
+	 *
+	 *所有表单提交均通过 Wnd_Form_Data 统一处理
+	 */
+	protected static function get_form_data() {
+		return Wnd_Form_Data::get_form_data(true);
+	}
 }
--- includes/utility/wnd-form-data.php
+++ includes/utility/wnd-form-data.php
@@ -2,14 +2,13 @@
 namespace Wnd\Utility;
 
 use Exception;
+use Wnd\Utility\Wnd_Validator;
 
 /**
  *根据表单name提取数据
  *@since 2019.03.04
  *
  *@param $verify_form_nonce 	bool 	是否校验表单字段经由Wnd_Form_WP表单类生成
- *@param $_POST 				array 	表单数据
- *
  *
  * 前端表单遵循以下规则定义的name，后台获取后自动提取，并更新到数据库
  *	文章：_post_{$field}
@@ -36,7 +35,22 @@ class Wnd_Form_Data {
 
 	public static $form_nonce_name = 'wnd_form_nonce';
 
+	/**
+	 *Construct
+	 */
 	public function __construct($verify_form_nonce = true) {
+		$this->form_data = static::get_form_data($verify_form_nonce);
+	}
+
+	/**
+	 *@since 0.8.64
+	 *获取经过基本校验解析的表单数据
+	 *
+	 *@param array 返回解析后的表单提交数据
+	 *			   与原$_POST相比，此时获取的表单提交数据，执行了 wnd_form_data filter，并通过了表单一致性校验、人机验证（根据表单设置）
+	 *			   请勿重复调用本方法
+	 */
+	public static function get_form_data(bool $verify_form_nonce): array{
 		if (empty($_POST)) {
 			throw new Exception(__('表单数据为空', 'wnd'));
 		}
@@ -51,18 +65,28 @@ class Wnd_Form_Data {
 			throw new Exception(__('表单已被篡改', 'wnd'));
 		}
 
+		/**
+		 *@since 0.8.64
+		 *人机验证：由于表单字段设置了字段名称一致性校验，前端无法更改字段，因此可用是否设置了 captcha 字段来判断当前表单是否需要人机验证
+		 */
+		if (isset($_POST['captcha'])) {
+			Wnd_Validator::validate_captcha();
+		}
+
 		// 允许修改表单提交数据
-		$this->form_data = apply_filters('wnd_form_data', $_POST);
+		$form_data = apply_filters('wnd_form_data', $_POST);
 
 		/**
 		 *根据表单数据控制表单提交
 		 *@since 2019.12.22
 		 *
 		 */
-		$can_array = apply_filters('wnd_can_submit_form', ['status' => 1], $this->form_data);
+		$can_array = apply_filters('wnd_can_submit_form', ['status' => 1], $form_data);
 		if (0 === $can_array['status']) {
 			throw new Exception($can_array['msg']);
 		}
+
+		return $form_data;
 	}
 
 	/**
@@ -121,16 +145,6 @@ class Wnd_Form_Data {
 	// 获取指定 option_name 数组数据
 	public function get_option_data($option_name): array{
 		return $this->get_data_by_prefix('_option_' . $option_name . '_');
-	}
-
-	/**
-	 *@since 2019.07.17
-	 *获取表单数据
-	 *返回表单提交数据
-	 *与原$_POST相比，此时获取的表单提交数据，执行了wnd_form_handler filter，并通过了表单一致性校验
-	 */
-	public function get_form_data(): array{
-		return $this->form_data;
 	}
 
 	/**
