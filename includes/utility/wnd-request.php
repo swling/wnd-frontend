850197324ffec9be6ffeff1ed1f44822a8b04c4f
Swling <tangfou@gmail.com>
2020-10-17 14:49:35 +0800

    Wnd_Request 命名优化

--- includes/utility/wnd-request.php
+++ includes/utility/wnd-request.php
@@ -6,12 +6,12 @@ use Wnd\Utility\Wnd_Captcha;
 use Wnd\Utility\Wnd_Validator;
 
 /**
- *根据表单name提取数据
  *@since 2019.03.04
  *
- *@param $verify_sign 	bool 	是否校验表单字段经由Wnd_Form_WP表单类生成
+ *@param $verify_sign 		bool 	是否校验签名
+ *@param $validate_captcha 	bool 	是否进行人机验证（如果存在）
  *
- * 前端表单遵循以下规则定义的name，后台获取后自动提取，并更新到数据库
+ * 前端请求遵循以下规则定义的name，后台获取后自动提取，并更新到数据库
  *	文章：_post_{$field}
  *
  * 	文章字段：
@@ -28,14 +28,13 @@ use Wnd\Utility\Wnd_Validator;
  *
  * option：
  * 存储在 Wnd option中 : _option_{$option_name}_{$option_key}
- *
  */
 class Wnd_Request {
 
 	/**
 	 *请求数据
 	 */
-	protected $data;
+	protected $request;
 
 	/**
 	 *签名请求 name
@@ -43,12 +42,12 @@ class Wnd_Request {
 	public static $sign_name = '_wnd_sign';
 
 	/**
-	 *解析表单数据时，是否验证表单签名
+	 *解析请求数据时，是否验证请求签名
 	 */
 	protected $verify_sign = true;
 
 	/**
-	 *解析表单数据时，是否进行人机验证（如果存在）
+	 *解析请求数据时，是否进行人机验证（如果存在）
 	 */
 	protected $validate_captcha = true;
 
@@ -59,84 +58,85 @@ class Wnd_Request {
 		$this->verify_sign      = $verify_sign;
 		$this->validate_captcha = $validate_captcha;
 
-		$this->parse_data();
+		$this->parse_request();
 	}
 
 	/**
 	 *@since 0.8.64
-	 *解析表单数据
+	 *解析请求数据
 	 *
-	 *@return array 返回解析后的表单提交数据
-	 *			   与原$_POST相比，此时获取的表单提交数据，执行了 wnd_request filter，并通过了表单一致性校验、人机验证（根据表单设置）
+	 *@return array 返回解析后的请求提交数据
+	 *			   与原请求数据相比，此时获取的请求提交数据，执行了 wnd_request_filter，并进行了签名验证及人机验证（数据验证根据设置可取消）
 	 *			   请勿重复调用本方法
 	 */
-	protected function parse_data(): array{
-		if (empty($_POST)) {
+	protected function parse_request(): array{
+		$request = ('POST' == $_SERVER['REQUEST_METHOD']) ? $_POST : $_GET;
+		if (empty($request)) {
 			return [];
 		}
 
 		/**
 		 *@since 2019.05.10
-		 *apply_filters('wnd_request', $_POST) 操作可能会直接修改$_POST
-		 *因而校验表单操作应该在filter应用之前执行
+		 *apply_filters('wnd_request', $request) 操作可能会直接修改$request
+		 *因而校验请求操作应该在filter应用之前执行
 		 *通过filter添加的数据，自动视为被允许提交的数据
 		 */
-		if ($this->verify_sign and !static::verify_sign()) {
+		if ($this->verify_sign and !static::verify_sign($request)) {
 			throw new Exception(__('数据已被篡改', 'wnd'));
 		}
 
 		/**
 		 *@since 0.8.64
-		 *人机验证：由于表单字段设置了字段名称一致性校验，前端无法更改字段，因此可用是否设置了 captcha 字段来判断当前表单是否需要人机验证
+		 *人机验证：由于请求字段设置了字段名称一致性校验，前端无法更改字段，因此可用是否设置了 captcha 字段来判断当前请求是否需要人机验证
 		 */
-		if ($this->validate_captcha and isset($_POST[Wnd_Captcha::$captcha_name])) {
+		if ($this->validate_captcha and isset($request[Wnd_Captcha::$captcha_name])) {
 			Wnd_Validator::validate_captcha();
 		}
 
-		// 允许修改表单提交数据
-		$data = apply_filters('wnd_request', $_POST);
+		// 允许修改请求提交数据
+		$request = apply_filters('wnd_request', $request);
 
 		/**
-		 *根据表单数据控制表单提交
+		 *根据请求数据控制请求提交
 		 *@since 2019.12.22
 		 *
 		 */
-		$can_array = apply_filters('wnd_request_controller', ['status' => 1], $data);
+		$can_array = apply_filters('wnd_request_controller', ['status' => 1], $request);
 		if (0 === $can_array['status']) {
 			throw new Exception($can_array['msg']);
 		}
 
-		$this->data = $data;
+		$this->request = $request;
 
-		return $this->data;
+		return $this->request;
 	}
 
 	/**
 	 *
-	 *获取表单数据
+	 *获取请求数据
 	 *
 	 *@since 0.8.73
 	 */
-	public function get_data(): array{
-		return $this->data ?: $this->parse_data();
+	public function get_request(): array{
+		return $this->request ?: $this->parse_request();
 	}
 
 	/**
 	 *
-	 *根据前缀提取指定表单数据
+	 *根据前缀提取指定请求数据
 	 *
 	 *@since 2020.01.04
 	 */
 	protected function get_data_by_prefix($prefix): array{
-		$data = [];
-		foreach ($this->data as $key => $value) {
+		$request = [];
+		foreach ($this->request as $key => $value) {
 			if (0 === strpos($key, $prefix)) {
-				$key        = str_replace($prefix, '', $key);
-				$data[$key] = $value;
+				$key           = str_replace($prefix, '', $key);
+				$request[$key] = $value;
 			}
 		}unset($key, $value);
 
-		return $data;
+		return $request;
 	}
 
 	// 获取WordPress user数据数组
@@ -180,59 +180,59 @@ class Wnd_Request {
 	}
 
 	/**
-	 *构造nonce表单字段
+	 *构造nonce请求字段
 	 *@since 2020.05.07
-	 *@param array 	$form_names 表单所有字段name数组
+	 *@param array 	$request_names 请求所有字段name数组
 	 */
-	public static function build_sign_field($form_names): string{
-		$nonce = static::sign($form_names);
+	public static function build_sign_field($request_names): string{
+		$nonce = static::sign($request_names);
 		return '<input type="hidden" name="' . static::$sign_name . '" value="' . $nonce . '">';
 	}
 
 	/**
-	 *构建表单字段
+	 *构建请求字段
 	 *
 	 *@since 2019.10.27
 	 *
-	 *@param array 	$form_names 表单所有字段name数组
+	 *@param array 	$request_names 请求所有字段name数组
 	 */
-	public static function sign(array $form_names): string{
-		// nonce 自身字段也需要包含在内，生成表单标识
-		$form_names[] = static::$sign_name;
-		$identifier   = static::generate_form_identifier($form_names);
+	public static function sign(array $request_names): string{
+		// nonce 自身字段也需要包含在内，生成请求标识
+		$request_names[] = static::$sign_name;
+		$identifier      = static::generate_request_identifier($request_names);
 
 		return wp_create_nonce($identifier);
 	}
 
 	/**
-	 *@since 2019.05.09 校验表单字段是否被篡改
+	 *@since 2019.05.09 校验请求字段是否被篡改
 	 *
 	 *@see static::sign
 	 */
-	public static function verify_sign(): bool {
-		if (!isset($_POST[static::$sign_name])) {
+	public static function verify_sign(array $request): bool {
+		if (!isset($request[static::$sign_name])) {
 			return false;
 		}
 
-		// 提取 $_POST $_FILES 数组键值，生成表单标识
-		$form_names = array_merge(array_keys($_POST), array_keys($_FILES));
-		$identifier = static::generate_form_identifier($form_names);
+		// 提取请求数据数组键值，生成请求标识
+		$request_names = array_merge(array_keys($request), array_keys($_FILES));
+		$identifier    = static::generate_request_identifier($request_names);
 
 		// 校验数组键值签名
-		return wp_verify_nonce($_POST[static::$sign_name], $identifier);
+		return wp_verify_nonce($request[static::$sign_name], $identifier);
 	}
 
 	/**
-	 *构建表单签名标识符
+	 *构建请求签名标识符
 	 *@since 0.8.66
 	 *
-	 *@param array 	$form_names 	表单字段数组
-	 *@param string 表单字段去重排序转为字符串后与结合 AUTH_KEY 生成表单标识码
+	 *@param array 	$request_names 	请求字段数组
+	 *@param string 请求字段去重排序转为字符串后与结合 AUTH_KEY 生成请求标识码
 	 */
-	protected static function generate_form_identifier(array $form_names): string{
-		$form_names = array_unique($form_names);
-		sort($form_names);
+	protected static function generate_request_identifier(array $request_names): string{
+		$request_names = array_unique($request_names);
+		sort($request_names);
 
-		return md5(implode('', $form_names) . AUTH_KEY);
+		return md5(implode('', $request_names) . AUTH_KEY);
 	}
 }
