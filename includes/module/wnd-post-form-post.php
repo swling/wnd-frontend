ee1125e71fa4b0b8d7986d79bac75e7a1d977510
Swling <tangfou@gmail.com>
2021-03-18 11:20:32 +0800

    优化：add_post_term_select

--- includes/module/wnd-post-form-post.php
+++ includes/module/wnd-post-form-post.php
@@ -55,18 +55,21 @@ class Wnd_Post_Form_Post extends Wnd_Module_Form {
 			$form->add_post_paid_file_upload();
 		}
 
-		$form->add_post_status_select();
 		// $form->add_html('</div>');
 
-		$form->add_html('<div class="field">' . wnd_modal_button(__('产品属性', 'wnd'), 'wnd_sku_form', ['post_id' => $form->get_post()->ID ?? 0]) . '</div>');
-		$form->add_html('<div class="field">');
-		$form->add_post_term_select(['taxonomy' => 'category'], '', true);
-		$form->add_html('</div>');
+		$form->add_html('
+<div class="field is-horizontal">
+<div class="field-label is-normal"><label class="label is-hidden-mobile">' . __('产品属性', 'wnd') . '</label></div>
+<div class="field-body"><div class="field">' . wnd_modal_button(__('产品 SKU', 'wnd'), 'wnd_sku_form', ['post_id' => $form->get_post()->ID ?? 0]) . '</div></div>
+</div>');
+
+		$form->add_post_term_select(['taxonomy' => 'category']);
 
 		// 缩略图
 		$form->set_thumbnail_size(150, 150);
 		$form->add_post_thumbnail(200, 200);
 		// $form->add_html('</div>');
+		$form->add_post_status_select();
 
 		$form->set_post_parent($post_parent ?: $form->get_post()->post_parent);
 		$form->set_submit_button(__('保存', 'wnd'));
--- includes/view/wnd-form-post.php
+++ includes/view/wnd-form-post.php
@@ -177,7 +177,7 @@ class Wnd_Form_Post extends Wnd_Form_WP {
 	}
 
 	// Term 分类单选下拉：本方法不支持复选
-	public function add_post_term_select($args_or_taxonomy, $label = '', $required = true) {
+	public function add_post_term_select($args_or_taxonomy, $label = 'default', $required = true) {
 		$taxonomy        = is_array($args_or_taxonomy) ? $args_or_taxonomy['taxonomy'] : $args_or_taxonomy;
 		$taxonomy_object = get_taxonomy($taxonomy);
 		if (!$taxonomy_object) {
@@ -195,7 +195,7 @@ class Wnd_Form_Post extends Wnd_Form_WP {
 				'options'  => $option_data,
 				'required' => $required,
 				'selected' => reset($this->current_terms[$taxonomy]) ?: '', //default checked value
-				'label'    => $label,
+				'label'    => ('default' == $label) ? $taxonomy_object->labels->name : $label,
 				'class'    => $taxonomy,
 				'data'     => ['child_level' => 0],
 			]
