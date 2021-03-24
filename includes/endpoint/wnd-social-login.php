6c382ac827653e72bd811212bc3b7e7932038be2
Swling <tangfou@gmail.com>
2021-03-24 16:53:52 +0800

    修复 bug：社交账户绑定无效

--- includes/endpoint/wnd-social-login.php
+++ includes/endpoint/wnd-social-login.php
@@ -1,6 +1,7 @@
 <?php
 namespace Wnd\Endpoint;
 
+use Exception;
 use Wnd\Utility\Wnd_Login_Social;
 
 /**
@@ -17,8 +18,20 @@ class Wnd_Social_Login extends Wnd_Endpoint {
 			return;
 		}
 
-		$domain       = Wnd_Login_Social::parse_state($this->data['state'])['domain'];
-		$Login_Social = Wnd_Login_Social::get_instance($domain);
-		$Login_Social->login();
+		/**
+		 *@since 0.9.27
+		 *本节点需做常规页面供外部回调。
+		 *故此手动添加通过 Cookie 设置当前用户，以维持账户登录状态，确保社交账号绑定，WP Nonce 校验等相关操作有效性
+		 */
+		$user_id = wp_validate_logged_in_cookie(0);
+		set_current_user($user_id);
+
+		try {
+			$domain       = Wnd_Login_Social::parse_state($this->data['state'])['domain'];
+			$Login_Social = Wnd_Login_Social::get_instance($domain);
+			$Login_Social->login();
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
 	}
 }
