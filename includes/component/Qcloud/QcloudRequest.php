cba95d2ab7565b84446b8b6b74a1c9010810e2f3
Swling <tangfou@gmail.com>
2021-03-25 12:39:02 +0800

    代码优化

--- includes/component/Qcloud/QcloudRequest.php
+++ includes/component/Qcloud/QcloudRequest.php
@@ -52,7 +52,7 @@ class QcloudRequest {
 		$request = wp_remote_post('https://' . $this->endpoint,
 			[
 				'body'    => $this->params,
-				'headers' => ['Content-type' => 'application/x-www-form-urlencoded;charset=UTF-8'],
+				'headers' => ['Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8'],
 			]
 		);
 		if (is_wp_error($request)) {
