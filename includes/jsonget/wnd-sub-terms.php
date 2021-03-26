a4b991c0c12252f20132789a5f8d46afe476853c
Swling <tangfou@gmail.com>
2021-03-26 16:29:38 +0800

    优化Jsonget ：Wnd_Sub_Terms

--- includes/jsonget/wnd-sub-terms.php
+++ includes/jsonget/wnd-sub-terms.php
@@ -11,13 +11,20 @@ class Wnd_Sub_Terms extends Wnd_JsonGet {
 
 	protected static function query($args = []): array{
 		$defaults = [
-			'taxonomy'   => 'category',
+			'taxonomy'   => '',
 			'parent'     => 0,
 			'hide_empty' => false,
 			'orderby'    => 'count',
 			'order'      => 'DESC',
 		];
 		$args = wp_parse_args($args, $defaults);
+
+		if (!$args['parent']) {
+			return [];
+		}
+
+		$args['taxonomy'] = $args['taxonomy'] ?: get_term($args['parent'])->taxonomy;
+
 		return Wnd_Term::get_terms_data($args);
 	}
 }
