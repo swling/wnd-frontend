fd4f363448d4d6513599b3945b9729d33f880761
Swling <tangfou@gmail.com>
2021-06-02 15:15:01 +0800

    修复支付宝当面付无法退款的 bug

---
+++ includes/getway/refund/alipay-qrcode.php
@@ -0,0 +1,11 @@
+<?php
+namespace Wnd\Getway\Refund;
+
+/**
+ *@since 0.9.30
+ *支付宝当面付退款（支付宝当面付退款与常规退款一致，继承即可）
+ *@link https://opendocs.alipay.com/apis/api_1/alipay.trade.refund
+ */
+class Alipay_QRCode extends Alipay {
+
+}
