55c557e853b3ed0bd7fc4b16410d90f194d9673c
Swling <tangfou@gmail.com>
2021-06-17 23:07:15 +0800

    修复取消订单时，SKU 库存计未能更新的 bug

--- includes/hook/wnd-add-action-wp.php
+++ includes/hook/wnd-add-action-wp.php
@@ -4,6 +4,7 @@ namespace Wnd\Hook;
 use Exception;
 use Wnd\Getway\Wnd_Captcha;
 use Wnd\Model\Wnd_Auth;
+use Wnd\Model\Wnd_Order_Product;
 use Wnd\Model\Wnd_Tag_Under_Category;
 use Wnd\Model\Wnd_User;
 use Wnd\Utility\Wnd_Defender_User;
@@ -11,7 +12,7 @@ use Wnd\Utility\Wnd_Singleton_Trait;
 use Wnd\Utility\Wnd_Validator;
 
 /**
- *WP Action
+ * WP Action
  */
 class Wnd_Add_Action_WP {
 
@@ -28,25 +29,24 @@ class Wnd_Add_Action_WP {
 		add_action('pre_get_posts', ['Wnd\View\Wnd_Filter', 'action_on_pre_get_posts'], 10, 1);
 
 		/**
-		 *@since 0.8.73
-		 *匿名用户评论验证码，基于 WordPress 原生评论表单及 wp_handle_comment_submission 评论提交
-		 *
+		 * 匿名用户评论验证码，基于 WordPress 原生评论表单及 wp_handle_comment_submission 评论提交
 		 * - 前端表单：@see comment_form(
 		 * - 提交验证：@see wp_handle_comment_submission
+		 * @since 0.8.73
 		 */
 		add_action('comment_form', [__CLASS__, 'action_on_comment_form'], 10, 1);
 		add_action('pre_comment_on_post', [__CLASS__, 'action_on_pre_comment_on_post'], 10, 1);
 
 		/**
-		 *分类关联标签
+		 * 分类关联标签
 		 */
 		Wnd_Tag_Under_Category::add_hook();
 	}
 
 	/**
-	 *@since 2020.04.30
-	 *This action hook is fired once WordPress, all plugins, and the theme are fully loaded and instantiated.
-	 *@link https://codex.wordpress.org/Plugin_API/Action_Reference/wp_loaded
+	 * This action hook is fired once WordPress, all plugins, and the theme are fully loaded and instantiated.
+	 * @link https://codex.wordpress.org/Plugin_API/Action_Reference/wp_loaded
+	 * @since 2020.04.30
 	 */
 	public static function action_on_wp_loaded() {
 		if (wnd_has_been_banned()) {
@@ -56,7 +56,7 @@ class Wnd_Add_Action_WP {
 	}
 
 	/**
-	 *@since 初始化 用户注册后
+	 * @since 初始化 用户注册后
 	 */
 	public static function action_on_user_register($user_id) {
 		// 注册类，将注册用户id写入对应数据表
@@ -71,8 +71,8 @@ class Wnd_Add_Action_WP {
 	}
 
 	/**
-	 *重设密码后
-	 *@since 0.8.62
+	 * 重设密码后
+	 * @since 0.8.62
 	 */
 	public static function action_on_password_reset($user) {
 		// Defender：重设密码后清空登录失败日志
@@ -81,8 +81,8 @@ class Wnd_Add_Action_WP {
 	}
 
 	/**
-	 *删除用户的附加操作
-	 *@since 2018
+	 * 删除用户的附加操作
+	 * @since 2018
 	 */
 	public static function action_on_delete_user($user_id) {
 		// 删除Wnd_User对象缓存
@@ -95,17 +95,16 @@ class Wnd_Add_Action_WP {
 	}
 
 	/**
-	 *@since 2019.03.28
-	 *删除文章时附件操作
-	 *
-	 *@since 2019.10.20
-	 *需要删除文章对应的子文章，需要定义在：before_delete_post，仅此时尚保留对应关系
+	 * 删除文章时附件操作
+	 * 需要删除文章对应的子文章，需要定义在：before_delete_post，仅此时尚保留对应关系
+	 * @since 2019.03.28
+	 * @since 2019.10.20
 	 */
 	public static function action_on_before_delete_post($post_id) {
 		$delete_post = get_post($post_id);
 
 		/**
-		 *删除附属文件
+		 * 删除附属文件
 		 */
 		$args = [
 			'posts_per_page' => -1,
@@ -121,12 +120,11 @@ class Wnd_Add_Action_WP {
 		unset($child);
 
 		/**
-		 *@since 2019.06.04 删除订单时，扣除订单统计字段
-		 *@since 2019.07.03 删除订单时，删除user_has_paid缓存
+		 * @since 2019.06.04 删除订单时，扣除订单统计字段
+		 * @since 2019.07.03 删除订单时，删除user_has_paid缓存
 		 */
 		if ('order' == $delete_post->post_type) {
-			wnd_inc_wnd_post_meta($delete_post->post_parent, 'order_count', -1, true);
-			wp_cache_delete($delete_post->post_author . '-' . $delete_post->post_parent, 'wnd_has_paid');
+			Wnd_Order_Product::cancel_order($delete_post);
 		}
 
 		/**
@@ -138,8 +136,8 @@ class Wnd_Add_Action_WP {
 	}
 
 	/**
-	 *@since 2019.06.05
-	 *文章更新
+	 * 文章更新
+	 * @since 2019.06.05
 	 */
 	public static function action_on_post_updated($post_ID, $post_after, $post_before) {
 		/**
@@ -160,22 +158,20 @@ class Wnd_Add_Action_WP {
 	}
 
 	/**
-	 *@since 2019.07.18
-	 *do_action( 'add_attachment', $post_ID );
-	 *新增上传附件时
+	 * do_action( 'add_attachment', $post_ID );
+	 * 新增上传附件时
+	 * @since 2019.07.18
 	 */
 	public static function action_on_add_attachment($post_ID) {
 		$post = get_post($post_ID);
 
 		/**
-		 *记录附件children_max_menu_order、删除附件时不做修改
-		 *记录值用途：读取后，自动给上传附件依次设置menu_order，以便按menu_order排序
-		 *@see wnd_filter_wp_insert_attachment_data
-		 *
-		 *
-		 *典型场景：
-		 *删除某个特定附件后，需要新上传附件，并恢复原有排序。此时要求新附件menu_order与删除的附件一致
-		 *通过wnd_attachment_form()上传文件，并编辑menu_order即可达到上述要求
+		 * 记录附件children_max_menu_order、删除附件时不做修改
+		 * 记录值用途：读取后，自动给上传附件依次设置menu_order，以便按menu_order排序
+		 * 典型场景：
+		 * 删除某个特定附件后，需要新上传附件，并恢复原有排序。此时要求新附件menu_order与删除的附件一致
+		 * 通过wnd_attachment_form()上传文件，并编辑menu_order即可达到上述要求
+		 * @see wnd_filter_wp_insert_attachment_data
 		 */
 		if ($post->post_parent) {
 			wnd_inc_wnd_post_meta($post->post_parent, 'attachment_records');
@@ -183,8 +179,8 @@ class Wnd_Add_Action_WP {
 	}
 
 	/**
-	 *@since 0.8.73
-	 *匿名用户提交评论验证码前端交互
+	 * 匿名用户提交评论验证码前端交互
+	 * @since 0.8.73
 	 */
 	public static function action_on_comment_form($post_ID) {
 		if (is_user_logged_in() or !wnd_get_config('captcha_service')) {
@@ -202,8 +198,8 @@ class Wnd_Add_Action_WP {
 	}
 
 	/**
-	 *@since 0.8.73
-	 *匿名用户提交评论验证码后端校验
+	 * 匿名用户提交评论验证码后端校验
+	 * @since 0.8.73
 	 */
 	public static function action_on_pre_comment_on_post($post_ID) {
 		if (is_user_logged_in() or !wnd_get_config('captcha_service')) {
--- includes/model/wnd-order-product.php
+++ includes/model/wnd-order-product.php
@@ -5,16 +5,13 @@ use Wnd\Model\Wnd_SKU;
 use Wnd\Model\Wnd_Transaction;
 
 /**
- *@since 0.9.0
- *
- *商品订单模块
- *
- *在本插件中，不单独对产品做定义。任何 singular 如 Post、Page 以及其他自定义 Post Type 均可为商品。一切皆可销售。
- *
- *注意：
+ * 商品订单模块
+ * 在本插件中，不单独对产品做定义。任何 singular 如 Post、Page 以及其他自定义 Post Type 均可为商品。一切皆可销售。
+ * 注意：
  *  产品配置将影响订单价格，因而不适用于付费阅读付费下载。
  *  因为付费阅读、付费下载，目前尚不支持指定 SKU 信息查询，仅通过判断用户在当前 Post 下是否有已完成支付的订单，决定内容呈现。
  *  简言之，付费阅读付费下载，应该设置唯一产品价格。
+ * @since 0.9.0
  */
 class Wnd_Order_Product {
 
@@ -31,11 +28,11 @@ class Wnd_Order_Product {
 	public static $ip_key = 'ip';
 
 	/**
-	 *设置订单关联的产品属性
+	 * 设置订单关联的产品属性
 	 *
-	 *读取数据中和产品属性相关的数据，保存至订单 wnd meta
-	 *由于sku_id 对应的产品信息可能发生改变，因此必须保存订单产生时的产品完整属性，以备后续核查
-	 *在产品单条 SKU 信息之外新增保存 SKU ID
+	 * 读取数据中和产品属性相关的数据，保存至订单 wnd meta
+	 * 由于sku_id 对应的产品信息可能发生改变，因此必须保存订单产生时的产品完整属性，以备后续核查
+	 * 在产品单条 SKU 信息之外新增保存 SKU ID
 	 */
 	public static function set_order_props(int $order_id, array $data): bool{
 		$meta      = [];
@@ -68,10 +65,10 @@ class Wnd_Order_Product {
 	}
 
 	/**
-	 *获取订单关联的产品属性
+	 * 获取订单关联的产品属性
 	 *
-	 *	订单属性，即从产品属性提供的选项中依次确定某一项组成。数据存储键名与产品属性保持一致。因此可复用 Wnd_Product::get_object_props($order_id);
-	 *	与产品属性返回的数据格式不同，【产品属性值】通常为维数组甚至二维数组，而【订单属性值】通常为确定的字符串。
+	 * 	订单属性，即从产品属性提供的选项中依次确定某一项组成。数据存储键名与产品属性保持一致。因此可复用 Wnd_Product::get_object_props($order_id);
+	 * 	与产品属性返回的数据格式不同，【产品属性值】通常为维数组甚至二维数组，而【订单属性值】通常为确定的字符串。
 	 *
 	 */
 	public static function get_order_props(int $order_id): array{
@@ -79,7 +76,7 @@ class Wnd_Order_Product {
 	}
 
 	/**
-	 *获取订单关联的产品 SKU 属性
+	 * 获取订单关联的产品 SKU 属性
 	 *
 	 */
 	public static function get_order_sku(int $order_id): array{
@@ -87,7 +84,7 @@ class Wnd_Order_Product {
 	}
 
 	/**
-	 *释放未支付的订单，已更新订单统计及库存
+	 * 释放未支付的订单，已更新订单统计及库存
 	 * - 删除15分钟前未完成的订单，并扣除订单统计
 	 */
 	public static function release_pending_orders(int $object_id) {
@@ -105,11 +102,8 @@ class Wnd_Order_Product {
 			],
 		];
 		foreach (get_posts($args) as $order) {
-			// 取消订单
-			static::cancel_order($order);
-
 			/**
-			 * 此处不直接修正order_count，而是在删除订单时，通过action修正order_count @see wnd_action_deleted_post
+			 * 此处不直接调用 static::cancel_order()，而是在删除订单时，通过action修正 @see wnd_action_deleted_post
 			 * 以此确保订单统计的准确性，如用户主动删除，或其他原因人为删除订单时亦能自动修正订单统计
 			 */
 			wp_delete_post($order->ID, true);
@@ -118,18 +112,30 @@ class Wnd_Order_Product {
 	}
 
 	/**
-	 *取消订单
+	 * 取消订单
 	 */
-	protected static function cancel_order(\WP_Post $order) {
-		// 取消行为仅针对状态为待处理的订单
-		if (Wnd_Transaction::$processing_status != $order->post_status) {
-			return false;
-		}
-
+	public static function cancel_order(\WP_Post $order) {
 		$object_id = $order->post_parent ?? 0;
 		if (!$object_id) {
 			return false;
 		}
+
+		/**
+		 * @since 2019.07.03 删除订单时，删除user_has_paid缓存
+		 */
+		wp_cache_delete($order->post_author . '-' . $object_id, 'wnd_has_paid');
+
+		/**
+		 *  订单及库存取消行为仅针对状态为待处理的订单
+		 */
+		if (Wnd_Transaction::$processing_status != $order->post_status) {
+			return false;
+		}
+
+		/**
+		 * @since 2019.06.04 扣除订单统计
+		 */
+		wnd_inc_wnd_post_meta($object_id, 'order_count', -1, true);
 
 		/**
 		 *  还原库存
