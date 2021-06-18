4c9f50a3f37fa5091d7ed2c1e24c8019bbd643fd
Swling <tangfou@gmail.com>
2021-06-18 12:27:30 +0800

    代码格式化

--- includes/model/wnd-user.php
+++ includes/model/wnd-user.php
@@ -2,16 +2,15 @@
 namespace Wnd\Model;
 
 /**
- *@since 2019.10.25
- *用户
+ * 用户
+ * @since 2019.10.25
  */
 class Wnd_User {
 
 	/**
-	 *@since 2019.11.06
-	 *获取自定义用户对象
-	 *
-	 *主要数据：user_id、email、phone……
+	 * 获取自定义用户对象
+	 * 主要数据：user_id、email、phone……
+	 * @since 2019.11.06
 	 */
 	public static function get_wnd_user($user_id) {
 		$user = wp_cache_get($user_id, 'wnd_users');
@@ -20,7 +19,7 @@ class Wnd_User {
 		}
 
 		/**
-		 *将用户所有绑定设备集合为一个对象
+		 * 将用户所有绑定设备集合为一个对象
 		 */
 		global $wpdb;
 		$user          = new \StdClass();
@@ -50,18 +49,18 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2019.07.23
-	 *根据第三方网站获取的用户信息，注册或者登录到WordPress站点
-	 *@param string $type 			第三方账号类型
-	 *@param string $open_id 		第三方账号openID
-	 *@param string $display_name 	用户名称
-	 *@param string $avatar_url 	用户外链头像
+	 * 根据第三方网站获取的用户信息，注册或者登录到WordPress站点
+	 * @since 2019.07.23
 	 *
-	 **/
+	 * @param string $type         			第三方账号类型
+	 * @param string $open_id      		第三方账号openID
+	 * @param string $display_name 	用户名称
+	 * @param string $avatar_url   	用户外链头像
+	 */
 	public static function social_login($type, $open_id, $display_name, $avatar_url) {
 		/**
-		 *@since 0.8.73
-		 *社交登录必须获取用户昵称
+		 * 社交登录必须获取用户昵称
+		 * @since 0.8.73
 		 */
 		if (!$display_name) {
 			exit(__('昵称无效', 'wnd'));
@@ -106,17 +105,15 @@ class Wnd_User {
 		wp_set_auth_cookie($user_id, true);
 
 		/**
-		 *@since 0.8.61
+		 * @since 0.8.61
 		 *
-		 *@param object WP_User
+		 * @param object WP_User
 		 */
 		do_action('wnd_login', $user);
 
 		/**
-		 * @see （本代码段从 wp_signon 复制而来)
-		 *
 		 * Fires after the user has successfully logged in.
-		 *
+		 * @see （本代码段从 wp_signon 复制而来)
 		 * @since 1.5.0
 		 *
 		 * @param string  $user_login Username.
@@ -129,9 +126,10 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2019.01.26 根据用户id获取号码
-	 *@param 	int 			$user_id
-	 *@return 	string|false 	用户手机号或false
+	 * @since 2019.01.26 根据用户id获取号码
+	 *
+	 * @param  	int          			$user_id
+	 * @return 	string|false 	用户手机号或false
 	 */
 	public static function get_user_phone($user_id) {
 		if (!$user_id) {
@@ -142,10 +140,11 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2019.11.06	根据用户id获取openid
-	 *@param 	int 			$user_id
-	 *@param 	string 			$type 第三方账号类型
-	 *@return 	string|false 	用户openid或false
+	 * @since 2019.11.06	根据用户id获取openid
+	 *
+	 * @param  	int          			$user_id
+	 * @param  	string       			$type                第三方账号类型
+	 * @return 	string|false 	用户openid或false
 	 */
 	public static function get_user_openid($user_id, $type) {
 		if (!$user_id) {
@@ -159,9 +158,10 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2019.01.28 根据邮箱，手机，或用户名查询用户
-	 *@param 	string 			$email_or_phone_or_login
-	 *@return 	object|false	WordPress user object on success
+	 * @since 2019.01.28 根据邮箱，手机，或用户名查询用户
+	 *
+	 * @param  	string                 			$email_or_phone_or_login
+	 * @return 	object|false	WordPress user object on success
 	 */
 	public static function get_user_by($email_or_phone_or_login) {
 		if (!$email_or_phone_or_login) {
@@ -169,24 +169,24 @@ class Wnd_User {
 		}
 
 		/**
-		 *邮箱
+		 * 邮箱
 		 */
 		if (is_email($email_or_phone_or_login)) {
 			return get_user_by('email', $email_or_phone_or_login);
 		}
 
 		/**
-		 *手机或登录名
+		 * 手机或登录名
 		 *
-		 *若当前字符匹配手机号码格式，则优先使用手机号查询
-		 *若查询到用户即返回
-		 *最后返回用户名查询结果
+		 * 若当前字符匹配手机号码格式，则优先使用手机号查询
+		 * 若查询到用户即返回
+		 * 最后返回用户名查询结果
 		 *
-		 *注意：
-		 *强烈建议禁止用户使用纯数字作为用户名
-		 *否则可能出现手机号码与用户名的混乱，造成同一个登录名，对应过个账户信息的问题
+		 * 注意：
+		 * 强烈建议禁止用户使用纯数字作为用户名
+		 * 否则可能出现手机号码与用户名的混乱，造成同一个登录名，对应过个账户信息的问题
 		 *
-		 *本插件已禁用纯数字用户名：@see wnd_ajax_reg()
+		 * 本插件已禁用纯数字用户名：@see wnd_ajax_reg()
 		 */
 		if (wnd_is_mobile($email_or_phone_or_login)) {
 			return static::get_user_by_openid('phone', $email_or_phone_or_login);
@@ -196,11 +196,12 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2019.07.11
-	 *根据openID获取WordPress用户，用于第三方账户登录
-	 *@param string 	$type 			第三方账号类型
-	 *@param string		openID
-	 *@return WP_User 	object|false 	（WordPress：get_user_by）
+	 * 根据openID获取WordPress用户，用于第三方账户登录
+	 * @since 2019.07.11
+	 *
+	 * @param  string           	$type        			第三方账号类型
+	 * @param  string		openID
+	 * @return WP_User          	object|false 	（WordPress：get_user_by）
 	 */
 	public static function get_user_by_openid($type, $open_id) {
 		$type        = strtolower($type);
@@ -225,12 +226,13 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2019.07.11
-	 *写入用户open id
-	 *@param 	int 	$user_id
-	 *@param 	string 	$type 			第三方账号类型
-	 *@param 	string 	$open_id
-	 *@return 	int 	$wpdb->insert
+	 * 写入用户open id
+	 * @since 2019.07.11
+	 *
+	 * @param  	int    	$user_id
+	 * @param  	string 	$type           			第三方账号类型
+	 * @param  	string 	$open_id
+	 * @return 	int    	$wpdb->insert
 	 */
 	public static function update_user_openid($user_id, $type, $open_id) {
 		global $wpdb;
@@ -272,11 +274,12 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 0.9.4
-	 *删除用户 open id
-	 *@param 	int 	$user_id
-	 *@param 	string 	$type 			第三方账号类型
-	 *@return 	int 	$wpdb->delete
+	 * 删除用户 open id
+	 * @since 0.9.4
+	 *
+	 * @param  	int    	$user_id
+	 * @param  	string 	$type           			第三方账号类型
+	 * @return 	int    	$wpdb->delete
 	 */
 	public static function delete_user_openid($user_id, $type) {
 		global $wpdb;
@@ -301,11 +304,12 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2019.07.11
-	 *更新用户电子邮箱 同时更新插件用户数据库email，及WordPress账户email
-	 *@param 	int 	$user_id
-	 *@param 	string 	$email
-	 *@return 	int 	$wpdb->insert
+	 * 更新用户电子邮箱 同时更新插件用户数据库email，及WordPress账户email
+	 * @since 2019.07.11
+	 *
+	 * @param  	int    	$user_id
+	 * @param  	string 	$email
+	 * @return 	int    	$wpdb->insert
 	 */
 	public static function update_user_email($user_id, $email) {
 		$db = static::update_user_openid($user_id, 'email', $email);
@@ -319,20 +323,22 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2019.07.11
-	 *写入用户手机号码
-	 *@param 	int 	$user_id
-	 *@param 	string 	$phone
-	 *@return 	int 	$wpdb->insert
+	 * 写入用户手机号码
+	 * @since 2019.07.11
+	 *
+	 * @param  	int    	$user_id
+	 * @param  	string 	$phone
+	 * @return 	int    	$wpdb->insert
 	 */
 	public static function update_user_phone($user_id, $phone) {
 		return static::update_user_openid($user_id, 'phone', $phone);
 	}
 
 	/**
-	 *@since 2019.11.06
-	 *更新缓存
-	 *@param object $user Wnd_user表对象
+	 * 更新缓存
+	 * @since 2019.11.06
+	 *
+	 * @param object $user Wnd_user表对象
 	 */
 	public static function update_wnd_user_caches(object $user_data) {
 		$user_id = $user_data->user_id ?? 0;
@@ -353,9 +359,10 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2019.11.06
-	 *删除缓存
-	 *@param object $user Wnd_user表对象
+	 * 删除缓存
+	 * @since 2019.11.06
+	 *
+	 * @param object $user Wnd_user表对象
 	 */
 	public static function clean_wnd_user_caches(object $user_data) {
 		$user_id = $user_data->user_id ?? 0;
@@ -375,17 +382,18 @@ class Wnd_User {
 	}
 
 	/**
-	 *根据类型构造 AUTH 对象缓存组
+	 * 根据类型构造 AUTH 对象缓存组
 	 */
 	protected static function get_auth_cache_group(string $type): string {
 		return 'wnd_auth_' . $type;
 	}
 
 	/**
-	 *@since 初始化 判断当前用户是否为管理员
-	 *@param 	int 	$user_id
-	 *@return 	bool
-	 *用户角色为：管理员或编辑 返回 true
+	 * 用户角色为：管理员或编辑 返回 true
+	 * @since 初始化 判断当前用户是否为管理员
+	 *
+	 * @param  	int    	$user_id
+	 * @return 	bool
 	 */
 	public static function is_manager($user_id = 0) {
 		$user = $user_id ? get_user_by('id', $user_id) : wp_get_current_user();
@@ -399,9 +407,10 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2020.04.30 判断当前用户是否已被锁定：wp user meta：status
-	 *@param 	int 	$user_id
-	 *@return 	bool
+	 * @since 2020.04.30 判断当前用户是否已被锁定：wp user meta：status
+	 *
+	 * @param  	int    	$user_id
+	 * @return 	bool
 	 */
 	public static function has_been_banned($user_id = 0) {
 		$user_id = $user_id ?: get_current_user_id();
@@ -411,11 +420,12 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 初始化
-	 *用户display name去重
-	 *@param 	string 		$display_name
-	 *@param 	int 		$exclude_id
-	 *@return 	int|false
+	 * 用户display name去重
+	 * @since 初始化
+	 *
+	 * @param  	string      		$display_name
+	 * @param  	int         		$exclude_id
+	 * @return 	int|false
 	 */
 	public static function is_name_duplicated($display_name, $exclude_id = 0) {
 		// 名称为空
@@ -434,12 +444,13 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2019.02.25
-	 *发送站内信
-	 *@param 	int 	$to 		收件人ID
-	 *@param 	string 	$subject 	邮件主题
-	 *@param 	string 	$message 	邮件内容
-	 *@return 	bool 	true on success
+	 * 发送站内信
+	 * @since 2019.02.25
+	 *
+	 * @param  	int    	$to      		收件人ID
+	 * @param  	string 	$subject 	邮件主题
+	 * @param  	string 	$message 	邮件内容
+	 * @return 	bool   	true on success
 	 */
 	public static function mail($to, $subject, $message) {
 		if (!get_user_by('id', $to)) {
@@ -466,9 +477,10 @@ class Wnd_User {
 	}
 
 	/**
-	 *获取最近的10封未读邮件
-	 *@since 2019.04.11
-	 *@return 	int 	用户未读邮件
+	 * 获取最近的10封未读邮件
+	 * @since 2019.04.11
+	 *
+	 * @return 	int 	用户未读邮件
 	 */
 	public static function get_mail_count() {
 		$user_id = get_current_user_id();
@@ -494,9 +506,10 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2019.06.10
-	 *获取用户面板允许的post types
-	 *@return array 	文章类型数组
+	 * 获取用户面板允许的post types
+	 * @since 2019.06.10
+	 *
+	 * @return array 	文章类型数组
 	 */
 	public static function get_user_panel_post_types() {
 		$post_types = get_post_types(['public' => true], 'names', 'and');
@@ -506,19 +519,18 @@ class Wnd_User {
 	}
 
 	/**
-	 *@since 2020.04.11
-	 *获取注册后跳转地址
+	 * 获取注册后跳转地址
+	 * @since 2020.04.11
 	 */
 	public static function get_reg_redirect_url() {
 		return wnd_get_config('reg_redirect_url') ?: home_url();
 	}
 
 	/**
-	 *@since 2020.04.11
-	 *获取用户语言
-	 *
-	 *该语言不同于WP原生的get_user_locale
-	 *WP原生存储与wp user meta；本插件存储与wnd user meta：目的是减少一行数据库记录
+	 * 获取用户语言
+	 * 该语言不同于WP原生的get_user_locale
+	 * WP原生存储与wp user meta；本插件存储与wnd user meta：目的是减少一行数据库记录
+	 * @since 2020.04.11
 	 */
 	public static function get_user_locale($user_id) {
 		return wnd_get_user_meta($user_id, 'locale') ?: 'default';
