20f6a6f45316645e2133f5bd437aaddb2df58b91
Swling <tangfou@gmail.com>
2021-08-23 12:05:09 +0800

    账户表单新增只读信息：当前账户ID

--- includes/module/wnd-account-form.php
+++ includes/module/wnd-account-form.php
@@ -5,7 +5,7 @@ use Exception;
 use Wnd\View\Wnd_Form_User;
 
 /**
- *@since 2019.01.23 用户更新账户表单
+ * @since 2019.01.23 用户更新账户表单
  */
 class Wnd_Account_Form extends Wnd_Module_Form {
 
@@ -14,7 +14,7 @@ class Wnd_Account_Form extends Wnd_Module_Form {
 		$enable_sms = wnd_get_config('enable_sms');
 
 		/**
-		 *如果当前账户为社交登录，账户设置前必须绑定邮箱或手机
+		 * 如果当前账户为社交登录，账户设置前必须绑定邮箱或手机
 		 */
 		if (!$user->data->user_email and !wnd_get_user_phone($user->data->ID)) {
 			$message = '<p>' . __('出于安全考虑，请绑定邮箱或手机') . '</p>';
@@ -25,10 +25,11 @@ class Wnd_Account_Form extends Wnd_Module_Form {
 				$message .= wnd_modal_button(__('绑定手机'), 'wnd_bind_phone_form');
 			}
 
-			throw new Exception(__($message, 'wnd'));
+			throw new Exception($message);
 		}
 
 		$form = new Wnd_Form_User(true);
+		$form->add_text(['value' => $user->ID, 'label' => 'ID', 'disabled' => true]);
 		$form->add_user_password(__('当前密码', 'wnd'), __('当前密码', 'wnd'));
 		$form->add_user_new_password(__('新密码', 'wnd'), __('新密码', 'wnd'));
 		$form->add_user_new_password_repeat(__('确认新密码', 'wnd'), __('确认新密码', 'wnd'));
@@ -37,15 +38,14 @@ class Wnd_Account_Form extends Wnd_Module_Form {
 		$form->set_filter(__CLASS__);
 
 		/**
-		 *@since 2019.09.19
-		 *绑定邮箱或手机
+		 * 绑定邮箱或手机
+		 * @since 2019.09.19
 		 */
 		$html = '<div class="has-text-centered mt-3">';
 		$html .= wnd_modal_link(__('邮箱设置', 'wnd'), 'wnd_bind_email_form') . ' | ';
 		$html .= $enable_sms ? (wnd_modal_link(__('手机设置', 'wnd'), 'wnd_bind_phone_form') . ' | ') : '';
 		$html .= wnd_modal_link(__('重置密码', 'wnd'), 'wnd_user_center', ['do' => 'reset_password']) . ' | ';
 		$html .= wnd_modal_link(__('解除绑定', 'wnd'), 'wnd_unbind_openid_form');
-
 		$html .= '</div>';
 
 		$form->add_html($html);
