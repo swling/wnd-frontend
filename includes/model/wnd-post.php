3efd3ee310bfeee9a97e763ece6db60f9b339930
Swling <tangfou@gmail.com>
2021-09-06 18:30:28 +0800

    优化按别名查询post：get_post_by_slug

--- includes/model/wnd-post.php
+++ includes/model/wnd-post.php
@@ -6,17 +6,16 @@ use Wnd\Model\Wnd_Term;
 class Wnd_Post {
 
 	/**
-	 *如果需要上传图片等，需要在提交文章之前，预先获取一篇文章
-	 *1、查询当前用户的草稿，如果存在获取最近一篇草稿用于编辑
-	 *2、若当前用户没有草稿，则查询其他用户超过一天未发布的自动草稿，并初始化标题、清除相关附件等
-	 *3、以上皆无结果，则创建一篇新文章
-	 *@since 2018.11.12
-	 *默认：获取当前用户，一天以前编辑过的文章（一天内更新过的文章表示正处于编辑状态）
+	 * 如果需要上传图片等，需要在提交文章之前，预先获取一篇文章
+	 * 1、查询当前用户的草稿，如果存在获取最近一篇草稿用于编辑
+	 * 2、若当前用户没有草稿，则查询其他用户超过一天未发布的自动草稿，并初始化标题、清除相关附件等
+	 * 3、以上皆无结果，则创建一篇新文章
+	 * 默认：获取当前用户，一天以前编辑过的文章（一天内更新过的文章表示正处于编辑状态）
 	 *
-	 *@param string 	$post_type 		类型
+	 * @since 2018.11.12
 	 *
-	 *@return int 		$post_id|0 		成功获取草稿返回ID，否则返回0
-	 *
+	 * @param  string 	$post_type  		类型
+	 * @return int    		$post_id|0 		成功获取草稿返回ID，否则返回0
 	 */
 	public static function get_draft(string $post_type): int{
 		$user_id = get_current_user_id();
@@ -26,9 +25,9 @@ class Wnd_Post {
 
 		/**
 		 * 写入及更新权限过滤
-		 * @since 0.9.27
 		 * 创建草稿时同步权限钩子：'wnd_can_insert_post'
 		 * @see Wnd\Action\Wnd_Insert_Post
+		 * @since 0.9.27
 		 */
 		$can_insert_post = apply_filters('wnd_can_insert_post', ['status' => 1, 'msg' => ''], $post_type, 0);
 		if (0 === $can_insert_post['status']) {
@@ -36,20 +35,20 @@ class Wnd_Post {
 		}
 
 		/**
-		 *@since 2019.02.19
 		 * 写入post type检测
-		 *@since 2019.7.17
-		 *attachment为附件类型，前端可能会允许编辑更新attachment post信息，但此种类型应该是上传文件后由WordPress自动创建
-		 *@see media_handle_upload()
+		 * attachment为附件类型，前端可能会允许编辑更新attachment post信息，但此种类型应该是上传文件后由WordPress自动创建
+		 * @see media_handle_upload()
+		 * @since 2019.02.19
+		 * @since 2019.7.17
 		 */
 		if (!in_array($post_type, static::get_allowed_post_types()) or 'attachment' == $post_type) {
 			return 0;
 		}
 
 		/**
-		 *查询当前用户否存在草稿，有则调用最近一篇草稿编辑
-		 *@see get_posts()
-		 *@see wp_update_post
+		 * 查询当前用户否存在草稿，有则调用最近一篇草稿编辑
+		 * @see get_posts()
+		 * @see wp_update_post
 		 */
 		$query_array = [
 			'post_status'    => 'auto-draft',
@@ -80,10 +79,10 @@ class Wnd_Post {
 		}
 
 		/**
-		 *当前用户没有草稿，查询其他用户超过指定时间未编辑的草稿
-		 *更新自动草稿时候，modified 不会变需要查询 post_date
-		 *@see get_posts()
-		 *@see wp_update_post
+		 * 当前用户没有草稿，查询其他用户超过指定时间未编辑的草稿
+		 * 更新自动草稿时候，modified 不会变需要查询 post_date
+		 * @see get_posts()
+		 * @see wp_update_post
 		 */
 		$date_query = [
 			[
@@ -128,8 +127,8 @@ class Wnd_Post {
 		}
 
 		/**
-		 *全站没有可用草稿，创建新文章用于编辑
-		 *@see wp_insert_post()
+		 * 全站没有可用草稿，创建新文章用于编辑
+		 * @see wp_insert_post()
 		 */
 		$post_id = wp_insert_post(
 			[
@@ -144,11 +143,12 @@ class Wnd_Post {
 	}
 
 	/**
-	 *@since 2019.12.22
-	 *批量设置文章meta
-	 *@param int 	$post_id
-	 *@param array 	$meta_data 	wnd meta array
-	 *@param array 	$wp_meta_data  wp meta array
+	 * 批量设置文章meta
+	 * @since 2019.12.22
+	 *
+	 * @param int   	$post_id
+	 * @param array 	$meta_data    	wnd meta array
+	 * @param array 	$wp_meta_data wp meta array
 	 */
 	public static function set_meta(int $post_id, array $meta_data, array $wp_meta_data) {
 		if (!get_post($post_id)) {
@@ -176,10 +176,11 @@ class Wnd_Post {
 	}
 
 	/**
-	 *@since 2019.12.22
-	 *批量设置文章terms
-	 *@param int 	$post_id
-	 *@param array 	$terms_data  	terms_array
+	 * 批量设置文章terms
+	 * @since 2019.12.22
+	 *
+	 * @param int   	$post_id
+	 * @param array 	$terms_data 	terms_array
 	 */
 	public static function set_terms(int $post_id, array $terms_data) {
 		if (!get_post($post_id)) {
@@ -196,14 +197,13 @@ class Wnd_Post {
 	}
 
 	/**
-	 *@since 初始化
-	 *标题去重
+	 * 标题去重
+	 * @since 初始化
 	 *
-	 *@param string 	$title
-	 *@param int 	$exclude_id
-	 *@param string 	$post_type
-	 *
-	 *@return int|false
+	 * @param  string      	$title
+	 * @param  int         	$exclude_id
+	 * @param  string      	$post_type
+	 * @return int|false
 	 */
 	public static function is_title_duplicated($title, $exclude_id = 0, $post_type = 'post') {
 		if (empty($title)) {
@@ -222,55 +222,52 @@ class Wnd_Post {
 	}
 
 	/**
-	 *@since 2019.02.17 根据post name 获取post
+	 * @since 2019.02.17 根据post name 获取post
 	 *
-	 *@param string $post_name
-	 *@param string $post_type
-	 *@param string || array $post_status
-	 *
-	 *@return object|null
+	 * @param  string        $post_name
+	 * @param  string        $post_type
+	 * @param  string|array  $post_status
+	 * @return object|null
 	 */
 	public static function get_post_by_slug($post_name, $post_type = 'post', $post_status = 'publish') {
 		global $wpdb;
 		$post_name = urlencode($post_name);
 
 		if (is_array($post_status)) {
-			$post = $wpdb->get_results(
+			return $wpdb->get_row(
 				$wpdb->prepare(
 					"SELECT * FROM $wpdb->posts WHERE post_name = %s AND post_type = %s AND post_status in ('" . implode("','", $post_status) . "') LIMIT 1",
 					$post_name,
 					$post_type
 				)
 			);
-		} elseif ('any' == $post_status) {
-			$post = $wpdb->get_results(
+		}
+
+		if ('any' == $post_status) {
+			return $wpdb->get_row(
 				$wpdb->prepare(
 					"SELECT * FROM $wpdb->posts WHERE post_name = %s AND post_type = %s LIMIT 1",
 					$post_name,
 					$post_type
 				)
 			);
-		} else {
-			$post = $wpdb->get_results(
-				$wpdb->prepare(
-					"SELECT * FROM $wpdb->posts WHERE post_name = %s AND post_type = %s AND post_status = %s LIMIT 1",
-					$post_name,
-					$post_type,
-					$post_status
-				)
-			);
 		}
 
-		if ($post) {
-			return $post[0];
-		}
-		return false;
+		return $wpdb->get_row(
+			$wpdb->prepare(
+				"SELECT * FROM $wpdb->posts WHERE post_name = %s AND post_type = %s AND post_status = %s LIMIT 1",
+				$post_name,
+				$post_type,
+				$post_status
+			)
+		);
 	}
 
 	/**
-	 *@since 2019.02.19
-	 *当前用户可以写入或管理的文章类型
-	 *@return array : post type name数组
+	 * 当前用户可以写入或管理的文章类型
+	 * @since 2019.02.19
+	 *
+	 * @return array : post type name数组
 	 */
 	public static function get_allowed_post_types(): array{
 		$post_types = get_post_types(['public' => true], 'names', 'and');
@@ -280,16 +277,16 @@ class Wnd_Post {
 	}
 
 	/**
-	 *@since 2020.05.20
-	 *获取revision ID
-	 *普通用户已公开发布的信息，如再次修改，将创建一个child post，并设置post meta。此revision不同于wp官方revision。
+	 * 获取revision ID
+	 * 普通用户已公开发布的信息，如再次修改，将创建一个child post，并设置post meta。此revision不同于wp官方revision。
+	 * @since 2020.05.20
 	 */
 	public static function get_revision_id($post_id): int {
 		if (!$post_id) {
 			return 0;
 		}
 
-		$args = array(
+		$args = [
 			'order'       => 'DESC',
 			'orderby'     => 'date ID',
 			'post_parent' => $post_id,
@@ -297,7 +294,7 @@ class Wnd_Post {
 			'post_status' => 'any',
 			'meta_key'    => static::get_revision_meta_key(),
 			'meta_value'  => 'true',
-		);
+		];
 
 		$revisions = get_posts($args);
 		if (!$revisions) {
@@ -308,9 +305,9 @@ class Wnd_Post {
 	}
 
 	/**
-	 *@since 2020.05.20
-	 *当前post 是否为自定义 revision
-	 *此revision不同于wp官方revision。
+	 * 当前post 是否为自定义 revision
+	 * 此revision不同于wp官方revision。
+	 * @since 2020.05.20
 	 */
 	public static function is_revision($post_id): bool {
 		if ('true' == get_post_meta($post_id, static::get_revision_meta_key(), true)) {
@@ -321,23 +318,23 @@ class Wnd_Post {
 	}
 
 	/**
-	 *定义revision meta key
-	 *@since 2020.05.20
+	 * 定义revision meta key
+	 * @since 2020.05.20
 	 */
 	public static function get_revision_meta_key(): string {
 		return '_wnd_revision';
 	}
 
 	/**
-	 *将当前post更改为指定 revision
-	 *该revision不同于WordPress原生revision
+	 * 将当前post更改为指定 revision
+	 * 该revision不同于WordPress原生revision
 	 *
-	 *将恢复 post 及 post_meta、terms
+	 * 将恢复 post 及 post_meta、terms
 	 *
-	 *@param int    $revision_id 将要恢复的版本ID
-	 *@param string $post_stauts 新的状态
+	 * @since 2020.05.20
 	 *
-	 *@since 2020.05.20
+	 * @param int    $revision_id 将要恢复的版本ID
+	 * @param string $post_stauts 新的状态
 	 */
 	public static function restore_post_revision($revision_id, $post_status) {
 		$revision = get_post($revision_id, ARRAY_A);
