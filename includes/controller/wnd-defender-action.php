2bfaeb6c2911641fd5596a2158d0b1550c7858c6
Swling <tangfou@gmail.com>
2021-11-04 17:16:38 +0800

    代码优化：移除命名空间中全局函数前缀反斜线

--- includes/controller/wnd-defender-action.php
+++ includes/controller/wnd-defender-action.php
@@ -85,31 +85,31 @@ class Wnd_Defender_Action {
 			'period'      => $wnd_action->period,
 			'max_actions' => $wnd_action->max_actions,
 		];
-		$defend_args = \apply_filters('wnd_action_defend_args', $defend_args, $wnd_action);
-		\extract($defend_args);
+		$defend_args = apply_filters('wnd_action_defend_args', $defend_args, $wnd_action);
+		extract($defend_args);
 
 		$this->period           = $period;
 		$this->max_actions      = $max_actions;
 		$this->should_be_defend = ($max_actions and $period);
 		$this->action           = $wnd_action::get_class_name();
-		$this->user_id          = \get_current_user_id();
-		$this->client_ip        = \wnd_get_user_ip();
+		$this->user_id          = get_current_user_id();
+		$this->client_ip        = wnd_get_user_ip();
 		$this->actions_log      = $this->get_actions_log();
 		$this->action_log       = $this->get_action_log();
 	}
 
 	private function get_actions_log(): array{
 		if ($this->user_id) {
-			$actions_log = \wnd_get_user_meta($this->user_id, static::$meta_key);
+			$actions_log = wnd_get_user_meta($this->user_id, static::$meta_key);
 		} else {
-			$actions_log = \wp_cache_get($this->client_ip, static::$meta_key);
+			$actions_log = wp_cache_get($this->client_ip, static::$meta_key);
 		}
 
-		$actions_log = \is_array($actions_log) ? $actions_log : [];
+		$actions_log = is_array($actions_log) ? $actions_log : [];
 
 		// 清理超期的日志
 		foreach ($actions_log as $action => $log) {
-			if (\time() - $log['time'] > $this->period) {
+			if (time() - $log['time'] > $this->period) {
 				unset($actions_log[$action]);
 			}
 
@@ -120,7 +120,7 @@ class Wnd_Defender_Action {
 
 	private function get_action_log(): array{
 		$action_log = $this->actions_log[$this->action] ?? [];
-		return \array_merge(static::$default_action_log, $action_log);
+		return array_merge(static::$default_action_log, $action_log);
 	}
 
 	/**
@@ -137,9 +137,9 @@ class Wnd_Defender_Action {
 		}
 
 		// 锁定时间范围内:
-		$time = $this->period - (\time() - $this->action_log['time']);
+		$time = $this->period - (time() - $this->action_log['time']);
 		if ($time > 0) {
-			throw new Exception(\sprintf(__('操作太频繁，请 %s 分钟后重试', 'wnd'), \ceil($time / 60)));
+			throw new Exception(sprintf(__('操作太频繁，请 %s 分钟后重试', 'wnd'), ceil($time / 60)));
 		}
 	}
 
@@ -153,19 +153,19 @@ class Wnd_Defender_Action {
 			return false;
 		}
 
-		if (\time() - $this->action_log['time'] < $this->period) {
+		if (time() - $this->action_log['time'] < $this->period) {
 			$this->action_log['count']++;
 		} else {
-			$this->action_log['time']  = \time();
+			$this->action_log['time']  = time();
 			$this->action_log['count'] = 1;
 		}
 
 		$this->actions_log[$this->action] = $this->action_log;
 
 		if ($this->user_id) {
-			\wnd_update_user_meta($this->user_id, static::$meta_key, $this->actions_log);
+			wnd_update_user_meta($this->user_id, static::$meta_key, $this->actions_log);
 		} else {
-			\wp_cache_set($this->client_ip, $this->actions_log, static::$meta_key, $this->period);
+			wp_cache_set($this->client_ip, $this->actions_log, static::$meta_key, $this->period);
 		}
 	}
 
@@ -174,9 +174,9 @@ class Wnd_Defender_Action {
 	 */
 	public function reset_log() {
 		if ($this->user_id) {
-			\wnd_delete_user_meta($this->user_id, static::$meta_key);
+			wnd_delete_user_meta($this->user_id, static::$meta_key);
 		} else {
-			\wp_cache_delete($this->client_ip, static::$meta_key);
+			wp_cache_delete($this->client_ip, static::$meta_key);
 		}
 	}
 }
