15b1516d882df50cfa49e9c62cb89402b2fe2643
Swling <tangfou@gmail.com>
2021-12-19 11:44:57 +0800

    优化：Module 管理员财务订单统计列表

--- includes/module/wnd-finance-list.php
+++ includes/module/wnd-finance-list.php
@@ -1,6 +1,8 @@
 <?php
 namespace Wnd\Module;
 
+use Exception;
+use Wnd\Model\Wnd_Transaction;
 use Wnd\View\Wnd_Filter_Ajax;
 
 /**
@@ -14,9 +16,23 @@ class Wnd_Finance_List extends Wnd_Module_Filter {
 		$filter = new Wnd_Filter_Ajax();
 		$filter->add_search_form();
 		$filter->add_post_type_filter(['order', 'recharge']);
-		$filter->add_post_status_filter([__('全部', 'wnd') => 'any', __('已完成', 'wnd') => 'wnd-completed', __('进行中', 'wnd') => 'wnd-processing']);
+		$filter->add_post_status_filter(
+			[
+				__('全部', 'wnd')  => 'any',
+				__('已完成', 'wnd') => Wnd_Transaction::$completed_status,
+				__('进行中', 'wnd') => Wnd_Transaction::$processing_status,
+				__('已关闭', 'wnd') => Wnd_Transaction::$closed_status,
+				__('已退款', 'wnd') => Wnd_Transaction::$refunded_status,
+			]
+		);
 		$filter->set_posts_per_page($this->args['posts_per_page']);
 		$filter->query();
 		return $filter->get_filter();
+	}
+
+	protected static function check($args) {
+		if (!wnd_is_manager()) {
+			throw new Exception(__('权限不足', 'wnd'));
+		}
 	}
 }
