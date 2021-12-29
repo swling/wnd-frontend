11aff756172a3b84109134d4ebbd8b4cb0bf8e4f
Swling <tangfou@gmail.com>
2021-12-29 12:31:12 +0800

    代码优化：Model\Wnd_User

--- includes/model/wnd-user.php
+++ includes/model/wnd-user.php
@@ -15,16 +15,89 @@ abstract class Wnd_User {
 	 * @since 2019.11.06
 	 */
 	public static function get_wnd_user($user_id): object{
-		$user = wp_cache_get($user_id, static::$user_cache_group);
+		$user = static::get_wnd_user_cache($user_id);
 		if ($user) {
 			return $user;
 		}
 
 		$user = static::get_db($user_id) ?: new \stdClass;
 		unset($user->ID);
-		static::update_wnd_user_caches($user);
+		static::update_wnd_user_cache($user);
 
 		return $user;
+	}
+
+	/**
+	 * 获取缓存
+	 * @param int $user_id
+	 */
+	private static function get_wnd_user_cache(int $user_id) {
+		return wp_cache_get($user_id, static::$user_cache_group);
+	}
+
+	/**
+	 * 更新缓存
+	 * @since 2019.11.06
+	 *
+	 * @param object $user Wnd_user表对象
+	 */
+	private static function update_wnd_user_cache(object $user_data) {
+		$user_id = $user_data->user_id ?? 0;
+		if (!$user_id) {
+			return false;
+		}
+
+		// 按 user id 缓存指定用户所有 auth 数据
+		wp_cache_set($user_id, $user_data, static::$user_cache_group);
+	}
+
+	/**
+	 * 删除缓存
+	 * @param int $user_id
+	 */
+	private static function delete_wnd_user_cache(int $user_id) {
+		wp_cache_delete($user_id, static::$user_cache_group);
+	}
+
+	/**
+	 * 记录登录日志
+	 * @since 0.9.57
+	 */
+	public static function write_login_log(): bool{
+		$user_id = get_current_user_id();
+		if (!$user_id) {
+			return false;
+		}
+
+		$db_records = static::get_wnd_user($user_id);
+		$last_login = $db_records->last_login ?? 0;
+		if ($last_login) {
+			$last_login = date('Y-m-d', $last_login);
+			if ($last_login == date('Y-m-d', time())) {
+				return false;
+			}
+		}
+
+		// 未设置登录时间/注册后未登录
+		static::update_db($user_id, ['last_login' => time(), 'client_ip' => wnd_get_user_ip()]);
+		return true;
+	}
+
+	/**
+	 * 获取长期未登录的睡眠账户
+	 * @since 0.9.57
+	 */
+	public static function get_sleep_users(int $day): array{
+		global $wpdb;
+		$timestamp = time() - (3600 * 24 * $day);
+		$results   = $wpdb->get_results(
+			$wpdb->prepare(
+				"SELECT * FROM $wpdb->wnd_users WHERE last_login < %d",
+				$timestamp
+			)
+		);
+
+		return $results ?: [];
 	}
 
 	/**
@@ -76,7 +149,7 @@ abstract class Wnd_User {
 		 */
 		if ($action) {
 			$user = (object) $data;
-			static::update_wnd_user_caches($user);
+			static::update_wnd_user_cache($user);
 		}
 
 		return $action;
@@ -104,74 +177,9 @@ abstract class Wnd_User {
 		);
 
 		if ($action) {
-			static::clean_wnd_user_caches($user_id);
+			static::delete_wnd_user_cache($user_id);
 		}
 
 		return $action;
-	}
-
-	/**
-	 * 更新缓存
-	 * @since 2019.11.06
-	 *
-	 * @param object $user Wnd_user表对象
-	 */
-	private static function update_wnd_user_caches(object $user_data) {
-		$user_id = $user_data->user_id ?? 0;
-		if (!$user_id) {
-			return false;
-		}
-
-		// 按 user id 缓存指定用户所有 auth 数据
-		wp_cache_set($user_id, $user_data, static::$user_cache_group);
-	}
-
-	/**
-	 * 删除缓存
-	 * @param int $user_id
-	 */
-	private static function clean_wnd_user_caches(int $user_id) {
-		wp_cache_delete($user_id, static::$user_cache_group);
-	}
-
-	/**
-	 * 记录登录日志
-	 * @since 0.9.57
-	 */
-	public static function write_login_log(): bool{
-		$user_id = get_current_user_id();
-		if (!$user_id) {
-			return false;
-		}
-
-		$db_records = static::get_wnd_user($user_id);
-		$last_login = $db_records->last_login ?? 0;
-		if ($last_login) {
-			$last_login = date('Y-m-d', $last_login);
-			if ($last_login == date('Y-m-d', time())) {
-				return false;
-			}
-		}
-
-		// 未设置登录时间/注册后未登录
-		static::update_db($user_id, ['last_login' => time(), 'client_ip' => wnd_get_user_ip()]);
-		return true;
-	}
-
-	/**
-	 * 获取长期未登录的睡眠账户
-	 * @since 0.9.57
-	 */
-	public static function get_sleep_users(int $day): array{
-		global $wpdb;
-		$timestamp = time() - (3600 * 24 * $day);
-		$results   = $wpdb->get_results(
-			$wpdb->prepare(
-				"SELECT * FROM $wpdb->wnd_users WHERE last_login < %d",
-				$timestamp
-			)
-		);
-
-		return $results ?: [];
 	}
 }
