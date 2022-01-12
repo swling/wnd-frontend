21055d84ed69842d3c6de515700291f30197a915
Swling <tangfou@gmail.com>
2022-01-12 18:41:30 +0800

    增强：Action\User\Wnd_Login 支持签发 Token

--- includes/action/user/wnd-login.php
+++ includes/action/user/wnd-login.php
@@ -3,6 +3,7 @@ namespace Wnd\Action\User;
 
 use Exception;
 use Wnd\Action\Wnd_Action;
+use Wnd\Utility\Wnd_JWT_handler;
 
 /**
  * @since 2019.1.13 用户登录
@@ -14,15 +15,62 @@ class Wnd_Login extends Wnd_Action {
 	private $remember;
 	private $redirect_to;
 	private $target_user;
+	private $type;
 
+	/**
+	 * 登录
+	 * - 设置当前用户
+	 * - App 登录返回 token
+	 * - 常规登录设置 cookie 并返回对应数据
+	 */
 	protected function execute(): array{
 		/**
-		 * 校验密码并登录
-		 *
-		 * - 之所以不采用 wp_signon，是因为我们需要对登录错误信息进行控制，如提示忘记密码的重设链接等
-		 * - 由于上述原因，我们从 wp_signon 复制了 do_action('wp_login', $user->user_login, $user);
-		 *   用于解决可能存在的钩子兼容问题
+		 * 设置当前用户
 		 */
+		wp_set_current_user($this->target_user->ID);
+
+		/**
+		 * App 登录返回 Token
+		 */
+		if ('token' == $this->type) {
+			$this->do_login_action();
+			return static::generate_token($this->target_user->ID);
+		}
+
+		/**
+		 * 常规网页登录
+		 */
+		wp_set_auth_cookie($this->target_user->ID, $this->remember);
+		$this->do_login_action();
+		if ($this->redirect_to) {
+			return ['status' => 3, 'msg' => __('登录成功', 'wnd'), 'data' => ['redirect_to' => $this->redirect_to, 'user_id' => $this->target_user->ID]];
+		} else {
+			return ['status' => 4, 'msg' => __('登录成功', 'wnd'), 'data' => ['user_id' => $this->target_user->ID]];
+		}
+	}
+
+	/**
+	 * 解析数据并核查密码
+	 */
+	protected function check() {
+		$this->username    = trim($this->data['_user_user_login']);
+		$this->password    = $this->data['_user_user_pass'];
+		$this->remember    = (bool) $this->data['remember'] ?? 0;
+		$this->redirect_to = $_REQUEST['redirect_to'] ?? home_url();
+		$this->type        = $this->data['type'] ?? '';
+
+		// 可根据邮箱，手机，或用户名查询用户
+		$this->target_user = wnd_get_user_by($this->username);
+		if (!$this->target_user) {
+			throw new Exception(__('用户不存在', 'wnd'));
+		}
+
+		// 登录过滤挂钩
+		$can_login = apply_filters('wnd_can_login', ['status' => 1, 'msg' => ''], $this->target_user);
+		if (0 === $can_login['status']) {
+			throw new Exception($can_login['msg']);
+		}
+
 		if (!wp_check_password($this->password, $this->target_user->data->user_pass, $this->target_user->ID)) {
 			/**
 			 * @since 0.8.61
@@ -33,10 +81,25 @@ class Wnd_Login extends Wnd_Action {
 
 			throw new Exception(__('密码错误', 'wnd'));
 		}
+	}
 
-		wp_set_current_user($this->target_user->ID);
-		wp_set_auth_cookie($this->target_user->ID, $this->remember);
+	/**
+	 * 生成 Token
+	 * @since 0.9.57.5
+	 */
+	private static function generate_token(int $user_id): array{
+		$jwt   = Wnd_JWT_Handler::get_instance();
+		$token = $jwt->generate_token($user_id);
+		$exp   = $jwt->parse_token($token)['exp'] ?? 0;
 
+		return ['token' => $token, 'exp' => $exp];
+	}
+
+	/**
+	 * 登录成功后需要执行的 Action
+	 * @since 0.9.57.5
+	 */
+	private function do_login_action() {
 		/**
 		 * @since 0.8.61
 		 *
@@ -53,30 +116,5 @@ class Wnd_Login extends Wnd_Action {
 		 * @param WP_User $user       WP_User object of the logged-in user.
 		 */
 		do_action('wp_login', $this->target_user->user_login, $this->target_user);
-
-		if ($this->redirect_to) {
-			return ['status' => 3, 'msg' => __('登录成功', 'wnd'), 'data' => ['redirect_to' => $this->redirect_to, 'user_id' => $this->target_user->ID]];
-		} else {
-			return ['status' => 4, 'msg' => __('登录成功', 'wnd'), 'data' => ['user_id' => $this->target_user->ID]];
-		}
-	}
-
-	protected function check() {
-		$this->username    = trim($this->data['_user_user_login']);
-		$this->password    = $this->data['_user_user_pass'];
-		$this->remember    = (bool) $this->data['remember'] ?? 0;
-		$this->redirect_to = $_REQUEST['redirect_to'] ?? home_url();
-
-		// 可根据邮箱，手机，或用户名查询用户
-		$this->target_user = wnd_get_user_by($this->username);
-		if (!$this->target_user) {
-			throw new Exception(__('用户不存在', 'wnd'));
-		}
-
-		// 登录过滤挂钩
-		$can_login = apply_filters('wnd_can_login', ['status' => 1, 'msg' => ''], $this->target_user);
-		if (0 === $can_login['status']) {
-			throw new Exception($can_login['msg']);
-		}
 	}
 }
