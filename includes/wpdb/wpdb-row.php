a568754c47702d876205dfbed33c835a1c6d0f9d
Swling <tangfou@gmail.com>
2022-11-19 18:00:09 +0800

    优化：WPDB_Row 缓存策略，支持多字段查询缓存

--- includes/wpdb/wpdb-row.php
+++ includes/wpdb/wpdb-row.php
@@ -6,7 +6,7 @@ use Exception;
 /**
  * # 单行数据表操作基类
  * - “增删改”操作仅针对单行
- * - 仅支持单行单字段查询
+ * - 仅支持单行查询，并统一缓存查询结果
  * - 在 wpdb 的基础上统一添加 Hook
  * - term_relationships 读取与其他表紧密关联，因此不适用于本类，对应为独立类 @see Term_Relationships_Handler();
  *
@@ -31,13 +31,6 @@ class WPDB_Row {
 	protected $object_name;
 	protected $primary_id_column;
 	protected $required_columns = [];
-
-	/**
-	 * 需要缓存的字段
-	 * 注意被缓存的字段应该是唯一值，否则可能导致缓存混乱
-	 * @see $this->maybe_set_data_into_cache()
-	 */
-	protected $object_cache_fields = [];
 
 	protected $wpdb;
 	protected $table;
@@ -101,36 +94,47 @@ class WPDB_Row {
 
 	/**
 	 * get data by column
-	 *（需要完善对象缓存，对于复杂查询统一调用 WP_XXX_Query，并在 WP_XXX_Query 中统一缓存）
 	 * @return object|false
 	 */
 	public function get_by(string $field, $value) {
-		$data = apply_filters("get_{$this->object_name}_data", false, $field, $value);
+		return $this->query([$field => $value]);
+	}
+
+	/**
+	 * query single row  by columns
+	 * @return object|false
+	 */
+	public function query(array $where) {
+		$data = apply_filters("get_{$this->object_name}_data", false, $where);
 		if (false !== $data) {
 			return $data;
 		}
+
+		// sql 语句
+		$conditions = '';
+		ksort($where);
+		foreach ($where as $field => $value) {
+			if (is_null($value)) {
+				$conditions .= "`$field` IS NULL";
+			} else {
+				$conditions .= "`$field` = " . "'{$value}'";
+			}
+		}
+		$sql       = "SELECT * FROM `$this->table` WHERE $conditions LIMIT 1";
+		$query_key = md5($sql);
 
 		// object cache
-		$data = $this->maybe_get_data_from_cache($field, $value);
+		$data = $this->get_data_from_cache($query_key);
 		if (false !== $data) {
 			return $data;
 		}
 
-		// sql 查询
-		if (is_null($value)) {
-			$conditions = "`$field` IS NULL";
-		} else {
-			$conditions = "`$field` = " . "'{$value}'";
-		}
-
-		$sql  = "SELECT * FROM `$this->table` WHERE $conditions LIMIT 1";
+		// get data form database success
 		$data = $this->wpdb->get_row($sql);
-
-		// get data success
 		if ($data) {
-			$this->maybe_set_data_into_cache($field, $value, $data);
+			$this->set_data_into_cache($query_key, $data);
 
-			do_action("get_{$this->object_name}_data_success", $data, $field, $value);
+			do_action("get_{$this->object_name}_data_success", $data, $where);
 		}
 
 		return $data ? (object) $data : false;
@@ -235,40 +239,52 @@ class WPDB_Row {
 	}
 
 	/**
-	 * maybe get data from cache
+	 * get data from cache
 	 */
-	private function maybe_get_data_from_cache(string $field, $value) {
-		$cache_group = $this->generate_filed_cache_group($field);
-		if (false === $cache_group) {
-			return false;
-		}
-
-		return wp_cache_get($value, $cache_group);
+	private function get_data_from_cache(string $query_key) {
+		$cache_group = $this->generate_cache_group();
+		return wp_cache_get($query_key, $cache_group);
 	}
 
 	/**
-	 * maybe set data into cache
+	 * set data into cache
 	 */
-	private function maybe_set_data_into_cache(string $field, $value, $data) {
-		$cache_group = $this->generate_filed_cache_group($field);
-		if (false === $cache_group) {
-			return false;
+	private function set_data_into_cache(string $query_key, $data) {
+		// 缓存查询结果
+		$cache_group = $this->generate_cache_group();
+		$cache_data  = wp_cache_set($query_key, $data, $cache_group);
+		if (!$cache_data) {
+			return;
 		}
 
-		return wp_cache_set($value, $data, $cache_group);
+		// 将查询缓存键作为值缓存，用于在数据删除时，删除当前行已经生成的所有查询缓存
+		$primary_id       = $this->primary_id_column;
+		$cache_keys_group = $this->generate_cache_keys_group();
+		$cache_keys       = wp_cache_get($data->$primary_id, $cache_keys_group) ?: [];
+		$cache_keys[]     = $query_key;
+		$cache_keys       = array_unique($cache_keys);
+
+		wp_cache_set($data->$primary_id, $cache_keys, $cache_keys_group);
 	}
 
 	/**
 	 * clean table cache When a row is deleted or updated
 	 */
 	public function clean_row_cache(object $old_data) {
-		foreach ($old_data as $field => $value) {
-			$cache_group = $this->generate_filed_cache_group($field);
-			if (false !== $cache_group) {
-				wp_cache_delete($value, $cache_group);
-			}
+		$primary_id       = $this->primary_id_column;
+		$cache_keys_group = $this->generate_cache_keys_group();
+		$cache_keys       = wp_cache_get($old_data->$primary_id, $cache_keys_group) ?: [];
+
+		// 删除已缓存的查询
+		$cache_group = $this->generate_cache_group();
+		foreach ($cache_keys as $query_key) {
+			wp_cache_delete($query_key, $cache_group);
 		}
 
+		// 删除对应主键已缓存的【查询键】
+		wp_cache_delete($old_data->$primary_id, $cache_keys_group);
+
+		// 更新表单更改时间
 		$this->refresh_db_table_last_changed();
 	}
 
@@ -276,19 +292,22 @@ class WPDB_Row {
 	 * Generate field cache group name
 	 * @return string|false
 	 */
-	private function generate_filed_cache_group(string $field): mixed {
-		if (!in_array($field, $this->object_cache_fields)) {
-			return false;
-		}
+	private function generate_cache_group(): string {
+		return $this->table_name;
+	}
 
-		return $this->table_name . ':' . $field;
+	/**
+	 * Generate field cache group name
+	 * @return string|false
+	 */
+	private function generate_cache_keys_group(): string {
+		return $this->table_name . '_cache_keys';
 	}
 
 	/**
 	 * Refresh last changed date for DB Table
 	 */
 	protected function refresh_db_table_last_changed(): bool {
-		// return wp_cache_delete_last_changed($this->table_name);
 		return wp_cache_delete('last_changed', $this->table_name);
 	}
 
