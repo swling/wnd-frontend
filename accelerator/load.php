553c3939f78a7608c3b070e23f3a2f2044f323ff
Swling <tangfou@gmail.com>
2023-08-09 09:20:40 +0800

    精简版：增加版本匹配功能

---
+++ accelerator/load.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @since 0.9.60.2
+ * 精简版版本匹配
+ *
+ * wp 内核每次更新都可能引起 wp-settings.php 改变。因此当版本更新后默认移除精简模式，直到核查无误后，更新本文件版本号
+ *
+ */
+
+require ABSPATH . 'wp-includes/version.php';
+global $wp_version;
+
+if ('6.3' == $wp_version) {
+	require __DIR__ . '/wp-settings.php';
+} else {
+	require ABSPATH . 'wp-settings.php';
+}
