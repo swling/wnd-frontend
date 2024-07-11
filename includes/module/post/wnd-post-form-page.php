e90a472f2a53cd132a50547997d94b65e750ceb1
Swling <tangfou@gmail.com>
2024-07-11 11:03:51 +0800

    page 表单：新增页面模板选择下拉菜单

--- includes/module/post/wnd-post-form-page.php
+++ includes/module/post/wnd-post-form-page.php
@@ -20,33 +20,44 @@ class Wnd_Post_Form_Page extends Wnd_Post_Form {
 		$args = wp_parse_args($args, $defaults);
 		extract($args);
 
-		/**
-		 * @since 2019.03.11 表单类
-		 */
 		$form = new Wnd_Form_Post('page', $post_id);
-
-		/**
-		 * 左侧栏
-		 */
 		$form->add_post_title(__('标题', 'wnd'));
 		$form->add_post_name();
 		$form->add_post_excerpt(__('摘要', 'wnd'));
-
-		// 相册
 		$form->set_thumbnail_size(100, 100);
 		// $form->add_post_gallery_upload(0, 0, __('相册图集', 'wnd'));
 		$form->add_post_price();
 
+		// 页面模板
+		$form->add_select(
+			[
+				'label'    => 'Template',
+				'options'  => static::get_page_template(),
+				'selected' => get_post_meta($post_id, '_wp_page_template', true) ?: '',
+				'name'     => '_wpmeta_' . '_wp_page_template',
+			]
+		);
+
 		$form->add_post_content(true);
 		$form->add_post_status_select();
-		$form->add_html('</div>');
-
 		$form->set_post_parent($post_parent ?: $form->get_post()->post_parent);
 		$form->set_submit_button(__('保存', 'wnd'));
 
 		// 以当前函数名设置filter hook
 		$form->set_filter(__CLASS__);
 		return $form;
+	}
+
+	private static function get_page_template(): array {
+		$templates = array_flip(wp_get_theme()->get_page_templates(null, 'page'));
+
+		ksort($templates);
+		$options = ['Default' => 'default'];
+		foreach (array_keys($templates) as $template) {
+			$options[$template] = esc_attr($templates[$template]);
+		}
+
+		return $options;
 	}
 
 }
