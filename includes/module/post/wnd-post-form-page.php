c22bef4068a51753293e915f92a9b6bc2d8f0fe2
Swling <tangfou@gmail.com>
2024-07-11 09:46:41 +0800

    调整：page 表单；用户内容管理状态新增：自动草稿

--- includes/module/post/wnd-post-form-page.php
+++ includes/module/post/wnd-post-form-page.php
@@ -11,7 +11,7 @@ class Wnd_Post_Form_Page extends Wnd_Post_Form {
 
 	public static $post_type = 'page';
 
-	protected static function configure_form(array $args = []): object{
+	protected static function configure_form(array $args = []): object {
 		$defaults = [
 			'post_id'     => 0,
 			'post_parent' => 0,
@@ -24,35 +24,23 @@ class Wnd_Post_Form_Page extends Wnd_Post_Form {
 		 * @since 2019.03.11 表单类
 		 */
 		$form = new Wnd_Form_Post('page', $post_id);
-		$form->add_html('<div class="columns post-form-post">');
 
 		/**
 		 * 左侧栏
 		 */
-		$form->add_html('<div class="column">');
-		$form->add_post_title();
+		$form->add_post_title(__('标题', 'wnd'));
 		$form->add_post_name();
-		$form->add_post_excerpt();
+		$form->add_post_excerpt(__('摘要', 'wnd'));
 
 		// 相册
 		$form->set_thumbnail_size(100, 100);
-		$form->add_post_gallery_upload(0, 0, __('相册图集', 'wnd'));
+		// $form->add_post_gallery_upload(0, 0, __('相册图集', 'wnd'));
+		$form->add_post_price();
 
-		/**
-		 * @since 2019.04 富媒体编辑器仅在非ajax请求中有效
-		 */
 		$form->add_post_content(true);
 		$form->add_post_status_select();
 		$form->add_html('</div>');
 
-		/**
-		 * 侧边栏
-		 */
-		$form->add_html('<div class="column is-2">');
-		$form->add_html('<div class="field">' . wnd_modal_button(__('产品属性', 'wnd'), 'common/wnd_sku_form', ['post_id' => $form->get_post()->ID ?? 0]) . '</div>');
-		$form->add_html('</div>');
-
-		$form->add_html('</div>');
 		$form->set_post_parent($post_parent ?: $form->get_post()->post_parent);
 		$form->set_submit_button(__('保存', 'wnd'));
 
@@ -60,4 +48,5 @@ class Wnd_Post_Form_Page extends Wnd_Post_Form {
 		$form->set_filter(__CLASS__);
 		return $form;
 	}
+
 }
--- includes/module/user/wnd-user-posts-panel.php
+++ includes/module/user/wnd-user-posts-panel.php
@@ -9,14 +9,22 @@ use Wnd\View\Wnd_Filter_Ajax;
  */
 class Wnd_User_Posts_Panel extends Wnd_Module_Filter {
 
-	protected function structure(): array{
+	protected function structure(): array {
 		$this->args['posts_per_page'] = $this->args['posts_per_page'] ?? get_option('posts_per_page');
 
 		$filter = new Wnd_Filter_Ajax();
 		$filter->remove_post_content();
 		// $filter->add_search_form();
 		$filter->add_post_type_filter(wnd_get_user_panel_post_types());
-		$filter->add_post_status_filter([__('发布', 'wnd') => 'publish', __('待审', 'wnd') => 'pending', __('关闭', 'wnd') => 'wnd-closed', __('草稿', 'wnd') => 'draft']);
+		$filter->add_post_status_filter(
+			[
+				__('发布', 'wnd') => 'publish',
+				__('待审', 'wnd') => 'pending',
+				__('关闭', 'wnd') => 'wnd-closed',
+				__('草稿', 'wnd') => 'draft',
+				'Auto Draft'      => 'auto-draft',
+			]
+		);
 		$filter->add_category_filter();
 		// $filter->add_tags_filter(10);
 		$filter->add_query_vars(['author' => get_current_user_id(), 'update_post_term_cache' => false, 'update_post_meta_cache' => false]);
