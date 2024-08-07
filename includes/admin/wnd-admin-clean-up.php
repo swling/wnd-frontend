b92e16c8c5e7e190a1a6e0522dcd70e2460b20f8
Swling <tangfou@gmail.com>
2024-08-07 08:02:48 +0800

    清理：超过一年的站内信

--- includes/admin/wnd-admin-clean-up.php
+++ includes/admin/wnd-admin-clean-up.php
@@ -2,6 +2,7 @@
 namespace Wnd\Admin;
 
 use Wnd\Model\Wnd_Transaction;
+use Wnd\WPDB\Wnd_Mail_DB;
 
 /**
  * 清理站点内容
@@ -33,6 +34,12 @@ class Wnd_Admin_Clean_UP {
 		$old_users = $wpdb->query(
 			"DELETE FROM $wpdb->wnd_auths WHERE user_id = 0 AND DATE_SUB(NOW(), INTERVAL 7 DAY) > FROM_UNIXTIME(time)"
 		);
+
+		// 一年前的站内信
+		$old_mails = $wpdb->get_col("SELECT ID FROM $wpdb->wnd_mails WHERE DATE_SUB(NOW(), INTERVAL 365 DAY) > FROM_UNIXTIME(sent_at)");
+		foreach ((array) $old_mails as $delete) {
+			Wnd_Mail_DB::get_instance()->delete($delete);
+		}
 
 		do_action('wnd_clean_up');
 		return true;
